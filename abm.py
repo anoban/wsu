be80bd4a3a157cdd5beba0daf4a42d0dd32f4666
anoban <2016sb117@univ.jfn.ac.lk>
2025-01-30 15:07:32 +1100

    modified:   abm.py

--- abm.py
+++ abm.py
@@ -21,6 +21,8 @@ class person(object):
         """ """
         return self._has_heard_rumour
 
+    # other alternatives to typing.Self is using a string literal "person" which will be eval()ed at runtime
+    # or import annotations from __future__ which turns all type annotations into string literals
     def converse(self, other: Self) -> None:
         """ """
         if other.has_rumour():
@@ -30,8 +32,9 @@ class person(object):
             pass
 
 
-MAX_CONTACTS: int = 11
+MAX_POPULATION_SIZE: int = 8_000_000
 MAX_ITERATIONS: int = 10_000
+MAX_CONTACTS: int = 11
 MAX_DAYS: int = 2_000
 
 
@@ -40,7 +43,7 @@ njit(fastmath=True)
 
 def main() -> None:
     """ """
-    np.array([])
+    population = np.empty(shape=(MAX_POPULATION_SIZE), dtype=person)
     print("Hello!")
 
 
