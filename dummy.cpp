760a76457e7de4cb2960a01e9bf26eeb41ba8b48
anoban <2016sb117@univ.jfn.ac.lk>
2025-02-22 07:07:39 +1100

    modified:   .gitignore 	deleted:    GRooTFullVersion.csv 	modified:   dummy.cpp

--- dummy.cpp
+++ dummy.cpp
@@ -1,9 +1,15 @@
 #include <algorithm>
 #include <cstdlib>
 #include <format>
+#include <iostream>
+#include <numeric>
+#include <ratio>
 #include <vector>
 
-auto main() -> int {
-    //
+auto wmain() -> int {
+    std::vector<float> randoms(100'000'000);
+    std::generate(randoms.begin(), randoms.end(), ::rand);
+    const auto sum = std::accumulate(randoms.cbegin(), randoms.cend(), 0.0L);
+    std::wcout << std::format(L"Sum is {:.10Lf}\n", sum);
     return EXIT_SUCCESS;
 }
