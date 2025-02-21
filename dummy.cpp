d87e6183cec6fe43f8620b902cdf282a1654645e
anoban <2016sb117@univ.jfn.ac.lk>
2025-02-22 07:30:27 +1100

    modified:   dummy.cpp

--- dummy.cpp
+++ dummy.cpp
@@ -1,5 +1,7 @@
 #include <algorithm>
 #include <cstdlib>
+#include <ctime>
+#include <execution>
 #include <format>
 #include <iostream>
 #include <numeric>
@@ -7,9 +9,12 @@
 #include <vector>
 
 auto wmain() -> int {
+    ::srand(::time(nullptr));
     std::vector<float> randoms(100'000'000);
-    std::generate(randoms.begin(), randoms.end(), ::rand);
-    const auto sum = std::accumulate(randoms.cbegin(), randoms.cend(), 0.0L);
+    std::generate(std::execution::parallel_unsequenced_policy {}, randoms.begin(), randoms.end(), []() noexcept -> float {
+        return ::rand() / static_cast<float>(RAND_MAX);
+    });
+    const auto&& sum = std::reduce(std::execution::parallel_unsequenced_policy {}, randoms.cbegin(), randoms.cend(), 0.0L);
     std::wcout << std::format(L"Sum is {:.10Lf}\n", sum);
     return EXIT_SUCCESS;
 }
