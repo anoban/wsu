15a902e4745a57e29e185aaa140cd613495029a2
anoban <2016sb117@univ.jfn.ac.lk>
2025-03-06 19:07:07 +1100

    modified:   .gitignore 	modified:   familynames.R 	new file:   plantlookup.csv

--- familynames.R
+++ familynames.R
@@ -1,4 +1,4 @@
-# aleready installed all these craps
+# have already installed all these craps
 
 # install.packages("devtools")
 # devtools::install_github("ropenscilabs/datastorr")
@@ -9,3 +9,9 @@ library(taxonlookup)
 # hate to admit but this works lol :(
 lookup_table(c("Pinus ponderosa","Quercus agrifolia"), by_species=TRUE)
 lookup_table(c("Cynodon dactylon", "Azadirachta indica"), by_species=TRUE)
+
+# let's serialize the lookup table so we don't have to use R again :)
+write.csv(data.frame(plant_lookup()), fileEncoding = "latin1",
+          file = "./plantlookup.csv", quote = FALSE, row.names = FALSE)
+
+# THAT'S ALL I NEEDED :)))
