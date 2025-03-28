54dbd282547973906a84fd59a7100430596f5555
anoban <2016sb117@univ.jfn.ac.lk>
2025-03-29 08:56:49 +1100

    modified:   GLMM - Wright Lab.ipynb 	new file:   LMM-Wright Lab.R 	new file:   networks.ipynb

---
+++ LMM-Wright Lab.R
@@ -0,0 +1,6 @@
+# 25/03/2025
+
+library(nlme) # Non Linear Mixed Effect models
+library(lme4) # Linear Mixed Effect models
+
+# RStudio is installed @ C:\Users\90963425\AppData\Local\Programs\RStudio WTF???
