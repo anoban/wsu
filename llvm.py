e1d47d1cdd7e129bb6cbc354249e7e1f2ed2976e
anoban <2016sb117@univ.jfn.ac.lk>
2025-03-31 08:54:30 +1100

    new file:   Maherali.etal.AmNat.Data.xlsx 	new file:   llvm.py

---
+++ llvm.py
@@ -0,0 +1,15 @@
+# download all major releases of LLVM
+
+
+LLVM_GITHUB_RELEASES_URL: str = "https://github.com/llvm/llvm-project/releases?page={:d}"
+GITHUB_RELEASES_PAGE_COUNT: int = 13
+
+
+def is_release_of_chosen_type() -> bool:
+    pass
+
+
+def harvest_win64_executable_download_url() -> str:
+    pass
+
+def
