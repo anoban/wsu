e73140702fa7803e9423d4dd869f8e5b9a18abf1
anoban <2016sb117@univ.jfn.ac.lk>
2025-04-12 12:23:34 +1000

    new file:   bayesian.py

---
+++ bayesian.py
@@ -0,0 +1,16 @@
+import ssl
+from urllib.request import Request, urlopen
+
+if __name__ == r"__main__":
+    ARCHIVE_BASE_URI: str = r"https://ia800702.us.archive.org/1/items/MIT18.650F16/"
+    LECTURE_VIDEO_TEMPLATE: str = r"MIT18_650F16_lec{:02d}_300k.mp4"
+
+    for i in range(1, 25):
+        opener = Request(ARCHIVE_BASE_URI + LECTURE_VIDEO_TEMPLATE.format(i))
+        opener.add_header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:137.0) Gecko/20100101 Firefox/137.0")
+        with urlopen(opener, context=ssl.SSLContext()) as request:
+            print(f"Downloading {LECTURE_VIDEO_TEMPLATE.format(i)}")
+            video: str = request.read()
+            with open(file=LECTURE_VIDEO_TEMPLATE.format(i), mode="w") as fp:
+                fp.write(video)
+            print("Downloading completed!")
