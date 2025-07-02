0c6a863f9cef26c37bbd8594954f8c9cbc9c5566
anoban <2016sb117@univ.jfn.ac.lk>
2025-07-02 12:25:18 +1000

    modified:   example.py

--- example.py
+++ example.py
@@ -16,26 +16,27 @@ class Net(nn.Module):
 
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.dropout1 = nn.Dropout(0.25)
-        self.dropout2 = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(9216, 128)
-        self.fc2 = nn.Linear(128, 10)
+
+        self.conv_01 = nn.Conv2d(1, 32, 3, 1)
+        self.conv_02 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout_01 = nn.Dropout(0.25)
+        self.dropout_02 = nn.Dropout(0.5)
+        self.fcon_01 = nn.Linear(9216, 128)
+        self.fcon_02 = nn.Linear(128, 10)
 
     @override
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.conv1(x)
+        x = self.conv_01(x)
         x = relu(x)
-        x = self.conv2(x)
+        x = self.conv_02(x)
         x = relu(x)
         x = max_pool2d(x, 2)
-        x = self.dropout1(x)
+        x = self.dropout_01(x)
         x = torch.flatten(x, 1)
-        x = self.fc1(x)
+        x = self.fcon_01(x)
         x = relu(x)
-        x = self.dropout2(x)
-        x = self.fc2(x)
+        x = self.dropout_02(x)
+        x = self.fcon_02(x)
         output = log_softmax(x, dim=1)
         return output
 
