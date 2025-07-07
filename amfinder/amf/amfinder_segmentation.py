82db9eff39f89f4cb3719a89a8d0518e2a0ebf2b
anoban <2016sb117@univ.jfn.ac.lk>
2025-07-07 14:17:42 +1000

    modified:   amfinder/amf/amfinder_save.py 	modified:   amfinder/amf/amfinder_segmentation.py 	modified:   amfinder/amf/amfinder_superresolution.py 	modified:   amfinder/amf/amfinder_train.py 	modified:   amfinder/amf/amfinder_zipfile.py 	deleted:    amfinder/amf/install.sh 	deleted:    amfinder/amf/requirements.txt

--- amfinder/amf/amfinder_segmentation.py
+++ amfinder/amf/amfinder_segmentation.py
@@ -36,29 +36,26 @@ Functions
 :function preprocess: Convert a tile list to NumPy array and normalise pixels.
 """
 
-import pyvips
 import random
+
+import pyvips
+
 random.seed(42)
-import numpy as np
-
-import amfinder_model as AmfModel
 import amfinder_config as AmfConfig
-
-
+import amfinder_model as AmfModel
+import numpy as np
 
 # Not the best quality, but optimized for speed.
 # To check availability, type in a terminal: vips -l interpolate
-INTERPOLATION = pyvips.vinterpolate.Interpolate.new('nearest')
+INTERPOLATION = pyvips.vinterpolate.Interpolate.new("nearest")
 
 
-
-def load(image_path, access='random'):
+def load(image_path, access="random"):
     """
     Loads an image using the vips library.
     """
 
     return pyvips.Image.new_from_file(image_path, access=access)
-
 
 
 def tile(image, r, c, edge=None):
@@ -74,63 +71,50 @@ def tile(image, r, c, edge=None):
     :rtype: list
     """
 
-    edge = edge if edge is not None else AmfConfig.get('tile_edge')
+    edge = edge if edge is not None else AmfConfig.get("tile_edge")
     tile = image.crop(c * edge, r * edge, edge, edge)
 
     # In super-resolution mode, ensure the tile is 42x42 pixels.
-    if AmfConfig.get('super_resolution'):
-      
+    if AmfConfig.get("super_resolution"):
         if edge != 42:
-        
             ratio = 42 / edge
             tile = tile.resize(ratio, interpolate=INTERPOLATION)
-    
+
     # Otherwise, use interpolation to bring tile to 126x126 pixels.
     elif AmfModel.INPUT_SIZE != edge:
-
         ratio = AmfModel.INPUT_SIZE / edge
         tile = tile.resize(ratio, interpolate=INTERPOLATION)
 
-    return np.ndarray(buffer=tile.write_to_memory(),
-                      dtype=np.uint8,
-                      shape=[tile.height, tile.width, tile.bands])
-
+    return np.ndarray(buffer=tile.write_to_memory(), dtype=np.uint8, shape=[tile.height, tile.width, tile.bands])
 
 
 def preprocess(tile_list):
     """
     Preprocess a list of tiles.
-    
+
     :param tile_list: list of tiles extracted using the function above.
     :return: a numpy array containing normalised pixel values for several tiles.
     :rtype: numpy.ndarray
     """
-    
+
     return np.array(tile_list, np.float32) / 255.0
 
 
-
 def mosaic(image, edge=None):
-
-    edge = edge if edge is not None else AmfConfig.get('tile_edge')
+    edge = edge if edge is not None else AmfConfig.get("tile_edge")
 
     nrows = int(image.height // edge)
     ncols = int(image.width // edge)
 
     if nrows == 0 or ncols == 0:
-
-        AmfLog.warning('Tile size ({edge} pixels) is too large')
+        AmfLog.warning("Tile size ({edge} pixels) is too large")
         return None
-        
-    else:
 
+    else:
         tiles = []
 
         for r in range(nrows):
-
             for c in range(ncols):
- 
                 tiles.append(tile(image, r, c, edge))
 
         return tiles
-
--- amfinder/amf/amfinder_train.py
+++ amfinder/amf/amfinder_train.py
@@ -22,7 +22,6 @@
 # IN THE SOFTWARE.
 
 
-
 """
 Neural network training.
 
@@ -53,37 +52,34 @@ Functions
 
 import io
 import os
-#import cv2
-import yaml
+import random
+
 import keras
 import psutil
-import random
+
+# import cv2
+import yaml
+
 random.seed(42)
-import pyvips
-import operator
 import functools
+import operator
+
+# from PIL import Image # debug
+from contextlib import redirect_stdout
+
+import amfinder_config as AmfConfig
+import amfinder_image as AmfImage
+import amfinder_log as AmfLog
+import amfinder_model as AmfModel
+import amfinder_save as AmfSave
+import amfinder_segmentation as AmfSegm
 import amfinder_zipfile as zf
 import numpy as np
 import pandas as pd
-#from PIL import Image # debug
-
-from contextlib import redirect_stdout
-
-from keras.callbacks import EarlyStopping
-from keras.callbacks import ReduceLROnPlateau
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau
 from keras.preprocessing.image import ImageDataGenerator
-
 from sklearn.model_selection import train_test_split
 from sklearn.utils.class_weight import compute_class_weight
-
-import amfinder_log as AmfLog
-import amfinder_plot as AmfPlot
-import amfinder_save as AmfSave
-import amfinder_image as AmfImage
-import amfinder_model as AmfModel
-import amfinder_config as AmfConfig
-import amfinder_segmentation as AmfSegm
-
 
 
 def get_zipfile(path):
@@ -96,8 +92,7 @@ def get_zipfile(path):
     :rtype: string
     """
 
-    return '{}.zip'.format(os.path.splitext(path)[0])
-
+    return "{}.zip".format(os.path.splitext(path)[0])
 
 
 def import_settings(path):
@@ -113,20 +108,16 @@ def import_settings(path):
     zfile = get_zipfile(path)
 
     try:
-
         assert zf.is_zipfile(zfile)
 
-        with zf.ZipFile(zfile, 'r') as z:
-
+        with zf.ZipFile(zfile, "r") as z:
             assert AmfSave.IMG_SETTINGS in z.namelist()
 
-            raw_text = z.read(AmfSave.IMG_SETTINGS).decode('utf-8')
+            raw_text = z.read(AmfSave.IMG_SETTINGS).decode("utf-8")
             return yaml.safe_load(raw_text)
 
     except AssertionError:
-
-        return {'tile_edge': AmfConfig.get('tile_edge')}
-
+        return {"tile_edge": AmfConfig.get("tile_edge")}
 
 
 def import_annotations(path):
@@ -142,23 +133,19 @@ def import_annotations(path):
     zfile = get_zipfile(path)
 
     try:
-
         assert zf.is_zipfile(zfile)
 
-        with zf.ZipFile(zfile, 'r') as z:
-
-            base = 'col' if AmfConfig.get('level') == 1 else 'myc'
-            table = f'{base}.tsv'
+        with zf.ZipFile(zfile, "r") as z:
+            base = "col" if AmfConfig.get("level") == 1 else "myc"
+            table = f"{base}.tsv"
 
             assert table in z.namelist()
 
-            raw_data = z.read(table).decode('utf-8')
-            return pd.read_csv(io.StringIO(raw_data), sep='\t')
+            raw_data = z.read(table).decode("utf-8")
+            return pd.read_csv(io.StringIO(raw_data), sep="\t")
 
     except AssertionError:
-
         return None
-
 
 
 def estimate_background_subsampling(input_dataset):
@@ -174,8 +161,7 @@ def estimate_background_subsampling(input_dataset):
     """
 
     # The concept of background tiles does not apply to CNN2.
-    if AmfConfig.get('level') == 2 or AmfConfig.get('drop') == 0:
-
+    if AmfConfig.get("level") == 2 or AmfConfig.get("drop") == 0:
         return 0
 
     # Counts annotations for each input table.
@@ -185,10 +171,10 @@ def estimate_background_subsampling(input_dataset):
     counts = functools.reduce(operator.add, count_list)
 
     # Number of tiles annotated as 'background/not a root/other'.
-    background = counts['X']
+    background = counts["X"]
 
     # Other tiles (i.e. annotated as 'colonised' or 'non-colonised' root).
-    other_counts = counts.drop(['row', 'col', 'X'])
+    other_counts = counts.drop(["row", "col", "X"])
 
     # Average tile count per annotation class.
     average = round(sum(other_counts.values) / len(other_counts))
@@ -197,58 +183,49 @@ def estimate_background_subsampling(input_dataset):
     excess = background - average
 
     if excess <= 0:
-
         return 0
 
     else:
-
         x = round(excess * 100 / background)
-        AmfLog.info(f'{x}% of background tiles will be ignored', indent=1)
+        AmfLog.info(f"{x}% of background tiles will be ignored", indent=1)
         return x
-
 
 
 def print_table_header():
     """
     Print table header.
     """
-    if AmfConfig.get('level') == 1:
-
-        print('Filename\tImage size (px)\tTile size (px)\tM+\tM-\tX\tDiscarded')
+    if AmfConfig.get("level") == 1:
+        print("Filename\tImage size (px)\tTile size (px)\tM+\tM-\tX\tDiscarded")
 
     else:
-
-        print('Filename\tImage size (px)\tTile size (px)\tA\tV\tH\tI')
-
+        print("Filename\tImage size (px)\tTile size (px)\tA\tV\tH\tI")
 
 
 def print_image_stats(path, image, config, annots, discarded):
     """
     Print image information.
     """
-    
-    edge = config['tile_edge']
+
+    edge = config["tile_edge"]
     base = os.path.basename(path)
     w = image.width
     h = image.height
 
-    if AmfConfig.get('level') == 1:
+    if AmfConfig.get("level") == 1:
+        y = annots["Y"].sum()
+        n = annots["N"].sum()
+        x = annots["X"].sum()
 
-        y = annots['Y'].sum()
-        n = annots['N'].sum()
-        x = annots['X'].sum()
-
-        print(f'{base}\t{w}x{h}\t{edge}\t{y}\t{n}\t{x}\t{discarded}', flush=True)
+        print(f"{base}\t{w}x{h}\t{edge}\t{y}\t{n}\t{x}\t{discarded}", flush=True)
 
     else:
-    
-        a = annots['A'].sum()
-        v = annots['V'].sum()
-        hy = annots['H'].sum()
-        i = annots['I'].sum()
-    
-        print(f'{base}\t{w}x{h}\t{edge}\t{a}\t{v}\t{hy}\t{i}', flush=True)
+        a = annots["A"].sum()
+        v = annots["V"].sum()
+        hy = annots["H"].sum()
+        i = annots["I"].sum()
 
+        print(f"{base}\t{w}x{h}\t{edge}\t{a}\t{v}\t{hy}\t{i}", flush=True)
 
 
 def load_dataset(input_files):
@@ -260,7 +237,7 @@ def load_dataset(input_files):
     :rtype: tuple
     """
 
-    print(f'[{AmfConfig.invite()}] Tile extraction.')
+    print(f"[{AmfConfig.invite()}] Tile extraction.")
 
     # Load image settings and annotations.
     settings = [import_settings(path) for path in input_files]
@@ -272,42 +249,34 @@ def load_dataset(input_files):
 
     # Terminate if there is no data to process.
     if len(filtered_dataset) == 0:
-
-        AmfLog.error('Input images do not contain tile annotations. '
-                     'Use amfbrowser to annotate tiles before training',
-                     AmfLog.ERR_NO_DATA)
+        AmfLog.error("Input images do not contain tile annotations. Use amfbrowser to annotate tiles before training", AmfLog.ERR_NO_DATA)
 
     # Determine the required amount of background subsampling (if active).
     subsampling = estimate_background_subsampling(filtered_dataset)
 
     tiles = []
     hot_labels = []
-    header = AmfConfig.get('header')
+    header = AmfConfig.get("header")
 
     print_table_header()
 
     for path, config, annots in filtered_dataset:
-
-        edge = config['tile_edge']
-        AmfConfig.set('tile_edge', edge)
+        edge = config["tile_edge"]
+        AmfConfig.set("tile_edge", edge)
 
         # FIXME: Random access is inefficient. To achieve better
         # efficiency we would have to load tiles row by row.
         image = AmfSegm.load(path)
-        
+
         # Extract tile sets (= original tile and augmented versions).
         # Repeat one-hot encoded annotations for each tile.
         discarded = 0
         for annot in annots.itertuples():
-
-            if AmfConfig.get('level') == 1 and subsampling > 0 and \
-               annot.X == 1 and random.uniform(0, 100) < subsampling:
-
+            if AmfConfig.get("level") == 1 and subsampling > 0 and annot.X == 1 and random.uniform(0, 100) < subsampling:
                 discarded += 1
                 pass
 
             else:
-
                 tile = AmfSegm.tile(image, annot.row, annot.col)
                 tiles.append(tile)
                 hot_labels.append(list(annot[3:]))
@@ -317,7 +286,6 @@ def load_dataset(input_files):
         del image
 
     return np.array(tiles, np.float32), np.array(hot_labels, np.uint8)
-
 
 
 def class_weights(one_hot_labels):
@@ -334,16 +302,13 @@ def class_weights(one_hot_labels):
     :rtype: dict
     """
 
-    print(f'[{AmfConfig.invite()}] Class weights')
+    print(f"[{AmfConfig.invite()}] Class weights")
 
     if AmfConfig.colonization():
-
         # For instance, [[0, 0, 1], [1, 0 , 0]] returns [2, 0]
         hot_indexes = np.argmax(one_hot_labels, axis=1)
         unique = np.unique(hot_indexes)
-        class_weights = compute_class_weight('balanced',
-                                             classes=unique,
-                                             y=hot_indexes)
+        class_weights = compute_class_weight("balanced", classes=unique, y=hot_indexes)
 
         # Annotation classes that do not ocurr in the input dataset.
         missing = [x for x in [0, 1, 2] if x not in unique]
@@ -354,34 +319,23 @@ def class_weights(one_hot_labels):
             class_weights = sorted(list(zip(all_indices, all_weights)))
             class_weights = [y for _, y in class_weights]
 
-        for cls, num, w  in zip(AmfConfig.get('header'),
-                                np.bincount(hot_indexes),
-                                class_weights):
-
+        for cls, num, w in zip(AmfConfig.get("header"), np.bincount(hot_indexes), class_weights):
             frac = int(round(100.0 * num / len(one_hot_labels)))
-            print(f'    - Class {cls}: {num} tiles ({frac}% of total).')
-            print(f'    - Training weight: %.2f' % (w))
+            print(f"    - Class {cls}: {num} tiles ({frac}% of total).")
+            print("    - Training weight: %.2f" % (w))
 
         return dict(enumerate(class_weights))
 
     else:
-
-        class_weights = [compute_class_weight('balanced',
-                                              classes=np.unique(y),
-                                              y=y) for y in one_hot_labels]
+        class_weights = [compute_class_weight("balanced", classes=np.unique(y), y=y) for y in one_hot_labels]
 
         sums = [np.bincount(x) for x in one_hot_labels]
-        for cls, ws, sums in zip(AmfConfig.get('header'), class_weights, sums):
-
-            print('    - ConvNet %s: %d active (weight: %.2f), '
-                  '%d inactive (weight: %.2f).' % (cls, sums[1], ws[1],
-                                                   sums[0], ws[0]))
+        for cls, ws, sums in zip(AmfConfig.get("header"), class_weights, sums):
+            print("    - ConvNet %s: %d active (weight: %.2f), %d inactive (weight: %.2f)." % (cls, sums[1], ws[1], sums[0], ws[0]))
 
         # Output format: {'A': {0: wA0, 1: wA1}, 'V': {0: wV0, 1:wV1}, ...}
         # where wA0, w1A, wV0, and wV1 are weights (cf. compute_class_weight).
-        return {x: dict(enumerate(y)) for x, y in zip(AmfConfig.get('header'),
-                                                      class_weights)}
-
+        return {x: dict(enumerate(y)) for x, y in zip(AmfConfig.get("header"), class_weights)}
 
 
 def get_callbacks():
@@ -394,24 +348,16 @@ def get_callbacks():
     """
 
     # Prevent overfitting and restores best weights.
-    e = EarlyStopping(monitor='val_loss',
-           min_delta=0,
-           patience=AmfConfig.get('patience'),
-           verbose=1,
-           mode='auto',
-           restore_best_weights=True)
-    AmfConfig.set('early_stopping', e)
+    e = EarlyStopping(
+        monitor="val_loss", min_delta=0, patience=AmfConfig.get("patience"), verbose=1, mode="auto", restore_best_weights=True
+    )
+    AmfConfig.set("early_stopping", e)
 
     # Reduce learning rate for fine tuning.
-    r = ReduceLROnPlateau(monitor='val_loss',
-           factor=0.2,
-           patience=2,
-           verbose=1,
-           min_lr=0.000001)
-    AmfConfig.set('reduce_lr_on_plateau', r)
+    r = ReduceLROnPlateau(monitor="val_loss", factor=0.2, patience=2, verbose=1, min_lr=0.000001)
+    AmfConfig.set("reduce_lr_on_plateau", r)
 
     return [e, r]
-
 
 
 class ImageDataGeneratorMO(ImageDataGenerator):
@@ -423,24 +369,17 @@ class ImageDataGeneratorMO(ImageDataGenerator):
     """
 
     def flow(self, x, y=None, **kwargs):
-        if y is None or \
-        not isinstance(y,list) or \
-        any([yy.ndim != 1 for yy in y]) or \
-        any([yy.size != len(x) for yy in y]):
-           raise ValueError('ImageDataGeneratorMO requires a '
-                            'list of outputs, each a 1D NumPy array of the '
-                             'same length as the input.')
+        if y is None or not isinstance(y, list) or any([yy.ndim != 1 for yy in y]) or any([yy.size != len(x) for yy in y]):
+            raise ValueError("ImageDataGeneratorMO requires a list of outputs, each a 1D NumPy array of the same length as the input.")
 
         y_sing = np.transpose(np.asarray(y))
 
-        generator_sing = super(ImageDataGeneratorMO,
-                               self).flow(x, y_sing, **kwargs)
+        generator_sing = super(ImageDataGeneratorMO, self).flow(x, y_sing, **kwargs)
 
         while True:
             batch_x, batch_y_sing = next(generator_sing)
             batch_y = [yy for yy in np.transpose(batch_y_sing)]
             yield batch_x, batch_y
-
 
 
 def save_model_architecture(model):
@@ -450,20 +389,16 @@ def save_model_architecture(model):
     :param model: Model to save.
     """
 
-    if AmfConfig.get('summary'):
+    if AmfConfig.get("summary"):
+        cnn = "CNN%d" % (AmfConfig.get("level"))
+        path = os.path.join(AmfConfig.get("outdir"), f"{cnn}_summary.txt")
 
-        cnn = 'CNN%d' % (AmfConfig.get('level'))
-        path = os.path.join(AmfConfig.get('outdir'), f'{cnn}_summary.txt')
-
-        with open(path, 'w') as sf:
-
+        with open(path, "w") as sf:
             with redirect_stdout(sf):
-
                 model.summary()
 
-        path = os.path.join(AmfConfig.get('outdir'), f'{cnn}_architecture.png')
+        path = os.path.join(AmfConfig.get("outdir"), f"{cnn}_architecture.png")
         keras.utils.plot_model(model, path, show_shapes=True)
-
 
 
 def print_memory_usage():
@@ -476,8 +411,8 @@ def print_memory_usage():
     print(f"* Total memory used: {mb} Mb.")
 
 
-
 img_index = 0
+
 
 def data_augm(tile):
     """
@@ -495,32 +430,24 @@ def data_augm(tile):
     num = random.randint(1, 4)
 
     if num == 1:
-
         output = AmfImage.invert(tile)
 
     elif num == 2:
-
         output = AmfImage.grayscale(tile)
 
     elif num == 3:
-
         output = AmfImage.rotate_colours(tile)
 
     else:
-
         output = tile
 
     # May randomly save some augmented tiles.
-    if img_index < AmfConfig.get('save_augmented_tiles') \
-    and bool(random.getrandbits(1)):
-
+    if img_index < AmfConfig.get("save_augmented_tiles") and bool(random.getrandbits(1)):
         im = Image.fromarray(output.astype(np.uint8))
         img_index += 1
-        im.save(os.path.join(AmfConfig.get('outdir'),
-                'tile_%06d.png' % (img_index)))
+        im.save(os.path.join(AmfConfig.get("outdir"), "tile_%06d.png" % (img_index)))
 
     return output
-
 
 
 def run(input_files):
@@ -543,41 +470,32 @@ def run(input_files):
     print_memory_usage()
 
     # Generates training and validation datasets.
-    xt, xc, yt, yc = train_test_split(tiles, labels,
-                                      shuffle=True,
-                                      test_size=AmfConfig.get('vfrac') / 100.0,
-                                      random_state=42)
+    xt, xc, yt, yc = train_test_split(tiles, labels, shuffle=True, test_size=AmfConfig.get("vfrac") / 100.0, random_state=42)
 
     t_gen = None
     v_gen = None
 
-    if AmfConfig.get('level') == 1:
-
+    if AmfConfig.get("level") == 1:
         # Root segmentation (colonized vs non-colonized vs background).
         # ConvNet I has a standard, single input/single output architecture,
         # and can use ImageDataGenerator.
-        if AmfConfig.get('data_augm'):
-            t_gen = ImageDataGenerator(rescale=1.0 / 255,
-                                       horizontal_flip=True,
-                                       vertical_flip=True,
-                                       brightness_range=[0.75, 1.25],
-                                       preprocessing_function=data_augm)
+        if AmfConfig.get("data_augm"):
+            t_gen = ImageDataGenerator(
+                rescale=1.0 / 255, horizontal_flip=True, vertical_flip=True, brightness_range=[0.75, 1.25], preprocessing_function=data_augm
+            )
         else:
             t_gen = ImageDataGenerator(rescale=1.0 / 255)
 
         v_gen = ImageDataGenerator(rescale=1.0 / 255)
 
     else:
-
         # AM fungal structures (arbuscules, vesicles, hyphae).
         # ConvNet II has multiple outputs. ImageDataGenerator is not suitable.
         # Reference: https://github.com/keras-team/keras/issues/3761
-        if AmfConfig.get('data_augm'):
-            t_gen = ImageDataGeneratorMO(rescale=1.0 / 255,
-                                         horizontal_flip=True,
-                                         vertical_flip=True,
-                                         brightness_range=[0.75, 1.25],
-                                         preprocessing_function=data_augm)
+        if AmfConfig.get("data_augm"):
+            t_gen = ImageDataGeneratorMO(
+                rescale=1.0 / 255, horizontal_flip=True, vertical_flip=True, brightness_range=[0.75, 1.25], preprocessing_function=data_augm
+            )
         else:
             t_gen = ImageDataGeneratorMO(rescale=1.0 / 255)
 
@@ -585,22 +503,24 @@ def run(input_files):
 
         # Reshape one-hot data in a way suitable for ImageDataGeneratorMO:
         # [[a1 v1 h1]...[aN vN hN]] -> [[a1...aN] [v1...vN] [h1...hN]]
-        nclasses = len(AmfConfig.get('header')) # (= 4)
+        nclasses = len(AmfConfig.get("header"))  # (= 4)
         yt = [np.array([x[i] for x in yt]) for i in range(nclasses)]
         yc = [np.array([x[i] for x in yc]) for i in range(nclasses)]
 
     # Determine weights to counteract class imbalance.
     weights = class_weights(yt)
 
-    bs = AmfConfig.get('batch_size')
+    bs = AmfConfig.get("batch_size")
 
-    his = model.fit(t_gen.flow(xt, yt, batch_size=bs),
-                    steps_per_epoch=len(xt) // bs,
-                    class_weight=weights,
-                    epochs=AmfConfig.get('epochs'),
-                    validation_data=v_gen.flow(xc, yc, batch_size=bs),
-                    validation_steps=len(xc) // bs,
-                    callbacks=get_callbacks(),
-                    verbose=2)
+    his = model.fit(
+        t_gen.flow(xt, yt, batch_size=bs),
+        steps_per_epoch=len(xt) // bs,
+        class_weight=weights,
+        epochs=AmfConfig.get("epochs"),
+        validation_data=v_gen.flow(xc, yc, batch_size=bs),
+        validation_steps=len(xc) // bs,
+        callbacks=get_callbacks(),
+        verbose=2,
+    )
 
     AmfSave.training_data(his.history, model)
