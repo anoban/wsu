3842f43d507999aceeaa3230f823da7335f9e7d9
anoban <2016sb117@univ.jfn.ac.lk>
2025-07-11 09:01:48 +1000

    modified:   models/mnistnn.py

--- models/mnistnn.py
+++ models/mnistnn.py
@@ -1,8 +1,6 @@
-from typing import override
-
 import torch
 import torch.nn as nn
-from torch.nn.functional import relu
+from torch.nn.functional import relu, softmax
 
 __doc__ = r"A collection of CNN classifiers with different architecures for MNIST style datasets"
 __all__ = (r"BareBonesNN",)
@@ -18,24 +16,22 @@ class BareBonesNN(nn.Module):
 
     """
 
-    def __init__(self, n_channels: int = 1, n_classes: int = 10) -> None:
+    def __init__(self, n_classes: int = 10) -> None:
         """ """
 
-        super(BareBonesNN, self).__init__()
+        super(BareBonesNN, self).__init__()  # type: ignore
 
-        self._nchannels: int = n_channels
-        self._nclasses: int = n_classes
         self._fucon_01 = nn.Linear(
             in_features=784,  # 28 x 28 pixels
             out_features=784 * 2,
         )
-        self._fucon_02 = nn.Linear(in_features=784 * 3, out_features=10)
+        self._fucon_02 = nn.Linear(in_features=784 * 3, out_features=n_classes)
 
-    @override
     def forwrad(self, image: torch.Tensor) -> torch.Tensor:
         image = self._fucon_01(image)
         image = relu(image)
         image = self._fucon_02(image)
+        return softmax(image)
 
     def learn(self) -> None:
         super().train(mode=True)
