9bdabdd9e504d2d54dd8f35fdaee38ceba661690
anoban <2016sb117@univ.jfn.ac.lk>
2025-07-10 15:33:05 +1000

    modified:   models/example.py 	modified:   models/mnist.py 	new file:   torch-basics.ipynb

--- models/mnist.py
+++ models/mnist.py
@@ -2,20 +2,20 @@ from typing import override
 
 import torch
 import torch.nn as nn
-from torch.nn.functional import relu
-from torch.optim import SGD
+from torch.nn.functional import log_softmax, max_pool2d, nll_loss, relu
+from torch.optim import SGD, Optimizer
 from torch.utils.data import DataLoader
 
 from lib import IdxDataset
 
 
-class ConvNNet(nn.Module):
+class RelNNet(nn.Module):
     """ """
 
     def __init__(self, n_channels: int, n_classes: int) -> None:
         """ """
 
-        super(ConvNNet, self).__init__()
+        super(RelNNet, self).__init__()
         self.__nchannels = n_channels  # number of colour channels
         self.__nclasses = n_classes  # number of image classes
 
@@ -26,10 +26,6 @@ class ConvNNet(nn.Module):
         self.__conv_02 = nn.Conv2d(
             in_channels=8, out_channels=16, kernel_size=(4, 4), stride=1
         )  # in channels of the next layer should match the out channels of the previous layer
-
-        # pooling layers
-        self.__maxpool = nn.MaxPool2d(kernel_size=(2, 2), stride=4)  # a 28 x 28 image will be transformed into a 14 x 14 image
-        self.__avgpool = nn.AvgPool2d(kernel_size=(2, 2), stride=4)  # a 14 x 14 image will be transformed into a 7 x 7 image
 
         # fully connected layers
         self.__fcon_01 = nn.Linear(
@@ -48,11 +44,11 @@ class ConvNNet(nn.Module):
         super().train(mode=True)  # set the base class on training mode
         _input = self.__conv_01(_input)  # apply the first convolution operation
         _input = relu(_input)  # activation
-        _input = self.__maxpool(_input)  # apply max pooling, 28 x 28 matrices will become 14 x 14 matrices
+        _input = nn.MaxPool2d(kernel_size=(2, 2), stride=4)(_input)  # apply max pooling, 28 x 28 matrices will become 14 x 14 matrices
 
         _input = self.__conv_02(_input)  # apply the second convolution
         _input = relu(_input)  # activation
-        _input = self.__avgpool(_input)  # apply average pooling, 14 x 14 matrices will become 7 x 7 matrices
+        _input = nn.AvgPool2d(kernel_size=(2, 2), stride=4)(_input)  # apply average pooling, 14 x 14 matrices will become 7 x 7 matrices
 
         # pass the result through the fully connected layers
         _input = self.__fcon_01(_input)
@@ -62,6 +58,87 @@ class ConvNNet(nn.Module):
         return _input
 
 
+class DropNNet(nn.Module):
+    """
+    a stripped down MNIST classifier example from https://github.com/pytorch/examples/blob/main/mnist/main.py
+    """
+
+    def __init__(self):
+        """ """
+
+        super(DropNNet, self).__init__()  # type: ignore - pyright keeps bitching
+
+        self.conv_01 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(3, 3), stride=1)
+        self.conv_02 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), stride=1)
+        self.fucon_01 = nn.Linear(in_features=9216, out_features=128)
+        self.fucon_02 = nn.Linear(in_features=128, out_features=10)
+
+    @override
+    def forward(self, _image: torch.Tensor) -> torch.Tensor:
+        """
+        carry out the forward pass of the data through the layers of the CNN, applying activation functions where appropriate.
+        """
+
+        _image = self.conv_01(_image)
+        _image = relu(_image)
+        _image = self.conv_02(_image)
+        _image = relu(_image)
+        _image = max_pool2d(_image, 2)
+        _image = nn.Dropout(0.25)(_image)
+        _image = torch.flatten(_image, 1)
+        _image = self.fucon_01(_image)
+        _image = relu(_image)
+        _image = nn.Dropout(0.5)(_image)
+        _image = self.fucon_02(_image)
+        output = log_softmax(_image, dim=1)
+        return output
+
+    def fit(self, train_loader: DataLoader[torch.Tensor], optimizer: Optimizer) -> None:
+        """
+        train the convolutional neural network with the provided dataset and optimizer.
+        """
+
+        super().train(mode=True)  # set the module on training mode
+
+        for data, label in train_loader:
+            optimizer.zero_grad()
+            output = self.forward(data)
+            loss = nll_loss(output, label)
+            loss.backward()
+            optimizer.step()
+
+    @torch.no_grad  # type: ignore
+    def predict(self, x: torch.Tensor) -> torch.Tensor:
+        """ """
+
+        super().eval()  # set the base class on evaluation mode, equivalent to super().train(mode=False)
+        return self.forward(x).argmax()
+
+    @torch.no_grad  # type: ignore
+    def evaluate(self, test_loader: DataLoader[torch.Tensor]) -> tuple[float, float]:
+        """
+        retruns tuple[float, float] - (average loss, accuracy score)
+        """
+
+        super().eval()  # set base class module on evaluation mode, equivalent to super().train(mode=False)
+
+        average_loss: float = 0.000
+        accuracy_score: float = 0.000
+
+        for data, label in test_loader:
+            data, label = data, label
+            output = self.forward(data)
+            average_loss += nll_loss(output, label, reduction="sum").item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            accuracy_score += pred.eq(label.view_as(pred)).sum().item()
+
+        # averaging
+        average_loss /= len(test_loader.dataset)
+        accuracy_score /= len(test_loader.dataset)
+
+        return (average_loss, accuracy_score)
+
+
 def main() -> None:
     train = IdxDataset(r"./FashionMNIST/train-labels-idx1-ubyte", r"./FashionMNIST/train-images-idx3-ubyte")
     test = IdxDataset(r"./FashionMNIST/t10k-labels-idx1-ubyte", r"./FashionMNIST/t10k-images-idx3-ubyte")
@@ -69,7 +146,7 @@ def main() -> None:
     train_loader = DataLoader(dataset=train, batch_size=1, shuffle=True, num_workers=6)
     test_loader = DataLoader(dataset=test, batch_size=1, shuffle=True, num_workers=6)
 
-    model = ConvNNet(n_channels=1, n_classes=10)
+    model = RelNNet(n_channels=1, n_classes=10)
 
     optimizer = SGD(params=model.parameters(), lr=0.001, momentum=0.900)
     criterion = nn.CrossEntropyLoss()
@@ -80,6 +157,27 @@ def main() -> None:
 
         loss = criterion(out, label)
         loss.backward()
+
+
+def main() -> None:
+    trainloader = DataLoader(
+        dataset=IdxDataset(idx3_filepath=r"./MNIST/train-images-idx3-ubyte", idx1_filepath=r"./MNIST/train-labels-idx1-ubyte"),
+        batch_size=1,
+        num_workers=8,
+    )
+    testloader = DataLoader(
+        dataset=IdxDataset(idx3_filepath=r"./MNIST/t10k-images-idx3-ubyte", idx1_filepath=r"./MNIST/t10k-labels-idx1-ubyte"),
+        batch_size=1,
+        num_workers=8,
+    )
+
+    model = DropNNet()
+    model.fit(trainloader, SGD(params=model.parameters(), lr=0.001))
+    model.evaluate(testloader)
+
+
+if __name__ == r"__main__":
+    main()
 
 
 if __name__ == r"__main__":
