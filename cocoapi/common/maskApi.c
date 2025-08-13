b08def19f21a8aec177b49fa653e9f03935af297
anoban <2016sb117@univ.jfn.ac.lk>
2025-08-13 12:46:10 +1000

    new file:   cocoapi/.vscode/settings.json 	modified:   cocoapi/PythonAPI/pycocotools/coco.py 	modified:   cocoapi/common/maskApi.c

--- cocoapi/common/maskApi.c
+++ cocoapi/common/maskApi.c
@@ -4,101 +4,73 @@
 * Code written by Piotr Dollar and Tsung-Yi Lin, 2015.
 * Licensed under the Simplified BSD License [see coco/license.txt]
 **************************************************************************/
-#pragma once
 
+// clang .\common\maskApi.c -Wall -Wextra -Werror -O3 -static -std=c23
+
+#include <assert.h>
 #include <math.h>
-#include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 
+#if (!defined _WIN32) || (!defined _WIN64)
+    #error This source has been customized only for Win32 systems!
+#endif
+
 typedef struct RLE {
-        uint64_t  h, w, m;
-        uint32_t* cnts;
+        unsigned long long h, w, m;
+        unsigned*          cnts;
 } RLE;
 
-/* Initialize/destroy RLE. */
-void rleInit(RLE* R, uint64_t h, uint64_t w, uint64_t m, uint32_t* cnts);
-void rleFree(RLE* R);
+static inline int __stdcall uintCompare(_In_ const void* const a, _In_ const void* const b) {
+    unsigned c = *((unsigned*) a), d = *((unsigned*) b);
+    return c > d ? 1 : c < d ? -1 : 0;
+}
 
-/* Initialize/destroy RLE array. */
-void rlesInit(RLE** R, uint64_t n);
-void rlesFree(RLE** R, uint64_t n);
+static inline unsigned __stdcall umin(_In_ unsigned a, _In_ unsigned b) { return (a < b) ? a : b; }
 
-/* Encode binary masks using RLE. */
-void rleEncode(RLE* R, const uint8_t* mask, uint64_t h, uint64_t w, uint64_t n);
+static inline unsigned __stdcall umax(_In_ unsigned a, _In_ unsigned b) { return (a > b) ? a : b; }
 
-/* Decode binary masks encoded via RLE. */
-void rleDecode(const RLE* R, uint8_t* mask, uint64_t n);
-
-/* Compute union or intersection of encoded masks. */
-void rleMerge(const RLE* R, RLE* M, uint64_t n, int intersect);
-
-/* Compute area of encoded masks. */
-void rleArea(const RLE* R, uint64_t n, uint32_t* a);
-
-/* Compute intersection over union between masks. */
-void rleIou(RLE* dt, RLE* gt, uint64_t m, uint64_t n, uint8_t* iscrowd, double* o);
-
-/* Compute non-maximum suppression between bounding masks */
-void rleNms(RLE* dt, uint64_t n, uint32_t* keep, double thr);
-
-/* Compute intersection over union between bounding boxes. */
-void bbIou(double* dt, double* gt, uint64_t m, uint64_t n, uint8_t* iscrowd, double* o);
-
-/* Compute non-maximum suppression between bounding boxes */
-void bbNms(double* dt, uint64_t n, uint32_t* keep, double thr);
-
-/* Get bounding boxes surrounding encoded masks. */
-void rleToBbox(const RLE* R, double* bb, uint64_t n);
-
-/* Convert bounding boxes to encoded masks. */
-void rleFrBbox(RLE* R, const double* bb, uint64_t h, uint64_t w, uint64_t n);
-
-/* Convert polygon to encoded mask. */
-void rleFrPoly(RLE* R, const double* xy, uint64_t k, uint64_t h, uint64_t w);
-
-/* Get compressed string representation of encoded mask. */
-char* rleToString(const RLE* R);
-
-/* Convert from compressed string representation of encoded mask. */
-void rleFrString(RLE* R, char* s, uint64_t h, uint64_t w);
-
-uint32_t umin(_In_ uint32_t a, _In_ uint32_t b) { return (a < b) ? a : b; }
-
-uint32_t umax(_In_ uint32_t a, _In_ uint32_t b) { return (a > b) ? a : b; }
-
-void rleInit(_Inout_ RLE* const R, uint64_t h, uint64_t w, uint64_t m, _In_ const uint32_t* const cnts) {
+/* Initialize RLE. */
+void __stdcall rleInit(
+    _Inout_ RLE* const R, unsigned long long h, unsigned long long w, unsigned long long m, _In_ const unsigned* const cnts
+) {
     R->h    = h;
     R->w    = w;
     R->m    = m;
-    R->cnts = !m ? NULL : malloc(sizeof(uint32_t) * m);
+    R->cnts = !m ? NULL : malloc(sizeof(unsigned) * m);
+    assert(R->cnts); //
 
     if (cnts)
-        for (uint64_t j = 0; j < m; j++) R->cnts[j] = cnts[j];
+        for (unsigned long long j = 0; j < m; j++) R->cnts[j] = cnts[j];
 }
 
-void rleFree(_Inout_ RLE* const R) {
+/* destroy RLE. */
+void __stdcall rleFree(_Inout_ RLE* const R) {
     free(R->cnts);
     R->cnts = NULL;
 }
 
-void rlesInit(RLE** R, uint64_t n) {
+/* Initialize RLE array. */
+void __stdcall rlesInit(RLE** R, unsigned long long n) {
     *R = malloc(sizeof(RLE) * n);
-    for (uint64_t i = 0; i < n; i++) rleInit((*R) + i, 0, 0, 0, 0);
+    for (unsigned long long i = 0; i < n; i++) rleInit((*R) + i, 0, 0, 0, 0);
 }
 
-void rlesFree(RLE** R, uint64_t n) {
-    for (uint64_t i = 0; i < n; i++) rleFree((*R) + i);
+/* destroy RLE array. */
+void rlesFree(RLE** R, unsigned long long n) {
+    for (unsigned long long i = 0; i < n; i++) rleFree((*R) + i);
     free(*R);
     *R = NULL;
 }
 
-void rleEncode(RLE* R, const uint8_t* M, uint64_t h, uint64_t w, uint64_t n) {
-    uint64_t i, j, k, a = w * h;
-    uint32_t c, *cnts;
-    uint8_t  p;
-    cnts = malloc(sizeof(uint32_t) * (a + 1));
+/* Encode binary masks using RLE. */
+void rleEncode(RLE* R, const unsigned char* M, unsigned long long h, unsigned long long w, unsigned long long n) {
+    unsigned long long i, j, k, a = w * h;
+    unsigned           c, *cnts;
+    unsigned char      p;
+    cnts = malloc(sizeof(unsigned) * (a + 1));
     for (i = 0; i < n; i++) {
-        const uint8_t* T = M + a * i;
+        const unsigned char* T = M + a * i;
         k = p = c = 0;
         for (j = 0; j < a; j++) {
             if (T[j] != p) {
@@ -114,10 +86,11 @@ void rleEncode(RLE* R, const uint8_t* M, uint64_t h, uint64_t w, uint64_t n) {
     free(cnts);
 }
 
-void rleDecode(const RLE* R, uint8_t* M, uint64_t n) {
-    uint64_t i, j, k;
+/* Decode binary masks encoded via RLE. */
+void rleDecode(const RLE* R, unsigned char* M, unsigned long long n) {
+    unsigned long long i, j, k;
     for (i = 0; i < n; i++) {
-        uint8_t v = 0;
+        unsigned char v = 0;
         for (j = 0; j < R[i].m; j++) {
             for (k = 0; k < R[i].cnts[j]; k++) *(M++) = v;
             v = !v;
@@ -125,11 +98,12 @@ void rleDecode(const RLE* R, uint8_t* M, uint64_t n) {
     }
 }
 
-void rleMerge(const RLE* R, RLE* M, uint64_t n, int intersect) {
-    uint32_t *cnts, c, ca, cb, cc, ct;
-    int       v, va, vb, vp;
-    uint64_t  i, a, b, h = R[0].h, w = R[0].w, m = R[0].m;
-    RLE       A, B;
+/* Compute union or intersection of encoded masks. */
+void rleMerge(const RLE* R, RLE* M, unsigned long long n, int intersect) {
+    unsigned *         cnts, c, ca, cb, cc, ct;
+    int                v, va, vb, vp;
+    unsigned long long i, a, b, h = R[0].h, w = R[0].w, m = R[0].m;
+    RLE                A, B;
     if (n == 0) {
         rleInit(M, 0, 0, 0, 0);
         return;
@@ -138,7 +112,7 @@ void rleMerge(const RLE* R, RLE* M, uint64_t n, int intersect) {
         rleInit(M, h, w, m, R[0].cnts);
         return;
     }
-    cnts = malloc(sizeof(uint32_t) * (h * w + 1));
+    cnts = malloc(sizeof(unsigned) * (h * w + 1));
     for (a = 0; a < m; a++) cnts[a] = R[0].cnts[a];
     for (i = 1; i < n; i++) {
         B = R[i];
@@ -186,18 +160,85 @@ void rleMerge(const RLE* R, RLE* M, uint64_t n, int intersect) {
     free(cnts);
 }
 
-void rleArea(const RLE* R, uint64_t n, uint32_t* a) {
-    uint64_t i, j;
+/* Compute area of encoded masks. */
+void rleArea(const RLE* R, unsigned long long n, unsigned* a) {
+    unsigned long long i, j;
     for (i = 0; i < n; i++) {
         a[i] = 0;
         for (j = 1; j < R[i].m; j += 2) a[i] += R[i].cnts[j];
     }
 }
 
-void rleIou(RLE* dt, RLE* gt, uint64_t m, uint64_t n, uint8_t* iscrowd, double* o) {
-    uint64_t g, d;
-    double * db, gb;
-    int      crowd;
+/* Get bounding boxes surrounding encoded masks. */
+void rleToBbox(const RLE* R, double* bb, unsigned long long n) {
+    unsigned long long i;
+    for (i = 0; i < n; i++) {
+        unsigned           h, w, x, y, xs, ys, xe, ye, xp, cc, t;
+        unsigned long long j, m;
+        h  = (unsigned) R[i].h;
+        w  = (unsigned) R[i].w;
+        m  = R[i].m;
+        m  = ((unsigned long long) (m / 2)) * 2;
+        xs = w;
+        ys = h;
+        xe = ye = 0;
+        cc      = 0;
+        if (!m) {
+            bb[4 * i + 0] = bb[4 * i + 1] = bb[4 * i + 2] = bb[4 * i + 3] = 0;
+            continue;
+        }
+        for (j = 0; j < m; j++) {
+            cc += R[i].cnts[j];
+            t   = cc - j % 2;
+            y   = t % h;
+            x   = (t - y) / h;
+            if (j % 2 == 0)
+                xp = x;
+            else if (xp < x) {
+                ys = 0;
+                ye = h - 1;
+            }
+            xs = umin(xs, x);
+            xe = umax(xe, x);
+            ys = umin(ys, y);
+            ye = umax(ye, y);
+        }
+        bb[4 * i + 0] = xs;
+        bb[4 * i + 2] = xe - xs + 1;
+        bb[4 * i + 1] = ys;
+        bb[4 * i + 3] = ye - ys + 1;
+    }
+}
+
+/* Compute intersection over union between bounding boxes. */
+void bbIou(double* dt, double* gt, unsigned long long m, unsigned long long n, unsigned char* iscrowd, double* o) {
+    double             h, w, i, u, ga, da;
+    unsigned long long g, d;
+    int                crowd;
+    for (g = 0; g < n; g++) {
+        double* G = gt + g * 4;
+        ga        = G[2] * G[3];
+        crowd     = iscrowd != NULL && iscrowd[g];
+        for (d = 0; d < m; d++) {
+            double* D    = dt + d * 4;
+            da           = D[2] * D[3];
+            o[g * m + d] = 0;
+            w            = fmin(D[2] + D[0], G[2] + G[0]) - fmax(D[0], G[0]);
+            if (w <= 0) continue;
+            h = fmin(D[3] + D[1], G[3] + G[1]) - fmax(D[1], G[1]);
+            if (h <= 0) continue;
+            i            = w * h;
+            u            = crowd ? da : da + ga - i;
+            o[g * m + d] = i / u;
+        }
+    }
+}
+
+/* Compute intersection over union between masks. */
+void rleIou(RLE* dt, RLE* gt, unsigned long long m, unsigned long long n, unsigned char* iscrowd, double* o) {
+    unsigned long long g, d;
+    double *           db, *gb;
+    int                crowd;
     db = malloc(sizeof(double) * m * 4);
     rleToBbox(dt, db, m);
     gb = malloc(sizeof(double) * n * 4);
@@ -213,9 +254,9 @@ void rleIou(RLE* dt, RLE* gt, uint64_t m, uint64_t n, uint8_t* iscrowd, double* 
                     o[g * m + d] = -1;
                     continue;
                 }
-                uint64_t ka, kb, a, b;
-                uint32_t c, ca, cb, ct, i, u;
-                int      va, vb;
+                unsigned long long ka, kb, a, b;
+                unsigned           c, ca, cb, ct, i, u;
+                int                va, vb;
                 ca = dt[d].cnts[0];
                 ka = dt[d].m;
                 va = vb = 0;
@@ -252,9 +293,10 @@ void rleIou(RLE* dt, RLE* gt, uint64_t m, uint64_t n, uint8_t* iscrowd, double* 
             }
 }
 
-void rleNms(RLE* dt, uint64_t n, uint32_t* keep, double thr) {
-    uint64_t i, j;
-    double   u;
+/* Compute non-maximum suppression between bounding masks */
+void rleNms(RLE* dt, unsigned long long n, unsigned* keep, double thr) {
+    unsigned long long i, j;
+    double             u;
     for (i = 0; i < n; i++) keep[i] = 1;
     for (i = 0; i < n; i++)
         if (keep[i]) {
@@ -266,32 +308,10 @@ void rleNms(RLE* dt, uint64_t n, uint32_t* keep, double thr) {
         }
 }
 
-void bbIou(double* dt, double* gt, uint64_t m, uint64_t n, uint8_t* iscrowd, double* o) {
-    double   h, w, i, u, ga, da;
-    uint64_t g, d;
-    int      crowd;
-    for (g = 0; g < n; g++) {
-        double* G = gt + g * 4;
-        ga        = G[2] * G[3];
-        crowd     = iscrowd != NULL && iscrowd[g];
-        for (d = 0; d < m; d++) {
-            double* D    = dt + d * 4;
-            da           = D[2] * D[3];
-            o[g * m + d] = 0;
-            w            = fmin(D[2] + D[0], G[2] + G[0]) - fmax(D[0], G[0]);
-            if (w <= 0) continue;
-            h = fmin(D[3] + D[1], G[3] + G[1]) - fmax(D[1], G[1]);
-            if (h <= 0) continue;
-            i            = w * h;
-            u            = crowd ? da : da + ga - i;
-            o[g * m + d] = i / u;
-        }
-    }
-}
-
-void bbNms(double* dt, uint64_t n, uint32_t* keep, double thr) {
-    uint64_t i, j;
-    double   u;
+/* Compute non-maximum suppression between bounding boxes */
+void bbNms(double* dt, unsigned long long n, unsigned* keep, double thr) {
+    unsigned long long i = 0, j = 0;
+    double             u = 0.000;
     for (i = 0; i < n; i++) keep[i] = 1;
     for (i = 0; i < n; i++)
         if (keep[i]) {
@@ -303,67 +323,13 @@ void bbNms(double* dt, uint64_t n, uint32_t* keep, double thr) {
         }
 }
 
-void rleToBbox(const RLE* R, double* bb, uint64_t n) {
-    uint64_t i;
-    for (i = 0; i < n; i++) {
-        uint32_t h, w, x, y, xs, ys, xe, ye, xp, cc, t;
-        uint64_t j, m;
-        h  = (uint32_t) R[i].h;
-        w  = (uint32_t) R[i].w;
-        m  = R[i].m;
-        m  = ((uint64_t) (m / 2)) * 2;
-        xs = w;
-        ys = h;
-        xe = ye = 0;
-        cc      = 0;
-        if (m == 0) {
-            bb[4 * i + 0] = bb[4 * i + 1] = bb[4 * i + 2] = bb[4 * i + 3] = 0;
-            continue;
-        }
-        for (j = 0; j < m; j++) {
-            cc += R[i].cnts[j];
-            t   = cc - j % 2;
-            y   = t % h;
-            x   = (t - y) / h;
-            if (j % 2 == 0)
-                xp = x;
-            else if (xp < x) {
-                ys = 0;
-                ye = h - 1;
-            }
-            xs = umin(xs, x);
-            xe = umax(xe, x);
-            ys = umin(ys, y);
-            ye = umax(ye, y);
-        }
-        bb[4 * i + 0] = xs;
-        bb[4 * i + 2] = xe - xs + 1;
-        bb[4 * i + 1] = ys;
-        bb[4 * i + 3] = ye - ys + 1;
-    }
-}
-
-void rleFrBbox(RLE* R, const double* bb, uint64_t h, uint64_t w, uint64_t n) {
-    uint64_t i;
-    for (i = 0; i < n; i++) {
-        double xs = bb[4 * i + 0], xe = xs + bb[4 * i + 2];
-        double ys = bb[4 * i + 1], ye = ys + bb[4 * i + 3];
-        double xy[8] = { xs, ys, xs, ye, xe, ye, xe, ys };
-        rleFrPoly(R + i, xy, 4, h, w);
-    }
-}
-
-int uintCompare(_In_ const void* const a, _In_ const void* const b) {
-    uint32_t c = *((uint32_t*) a), d = *((uint32_t*) b);
-    return c > d ? 1 : c < d ? -1 : 0;
-}
-
-void rleFrPoly(RLE* R, const double* xy, uint64_t k, uint64_t h, uint64_t w) {
+/* Convert polygon to encoded mask. */
+void rleFrPoly(RLE* R, const double* xy, unsigned long long k, unsigned long long h, unsigned long long w) {
     /* upsample and get discrete points densely along entire boundary */
-    uint64_t  j, m = 0;
-    double    scale = 5;
-    int *     x, *y, *u, *v;
-    uint32_t *a, *b;
+    unsigned long long j, m = 0;
+    double             scale = 5;
+    int *              x, *y, *u, *v;
+    unsigned *         a, *b;
     x = malloc(sizeof(int) * (k + 1));
     y = malloc(sizeof(int) * (k + 1));
     for (j = 0; j < k; j++) x[j] = (int) (scale * xy[j * 2 + 0] + .5);
@@ -431,21 +397,21 @@ void rleFrPoly(RLE* R, const double* xy, uint64_t k, uint64_t h, uint64_t w) {
         }
     /* compute rle encoding given y-boundary points */
     k = m;
-    a = malloc(sizeof(uint32_t) * (k + 1));
-    for (j = 0; j < k; j++) a[j] = (uint32_t) (x[j] * (int) (h) + y[j]);
-    a[k++] = (uint32_t) (h * w);
+    a = malloc(sizeof(unsigned) * (k + 1));
+    for (j = 0; j < k; j++) a[j] = (unsigned) (x[j] * (int) (h) + y[j]);
+    a[k++] = (unsigned) (h * w);
     free(u);
     free(v);
     free(x);
     free(y);
-    qsort(a, k, sizeof(uint32_t), uintCompare);
-    uint32_t p = 0;
+    qsort(a, k, sizeof(unsigned), uintCompare);
+    unsigned p = 0;
     for (j = 0; j < k; j++) {
-        uint32_t t  = a[j];
+        unsigned t  = a[j];
         a[j]       -= p;
         p           = t;
     }
-    b = malloc(sizeof(uint32_t) * k);
+    b = malloc(sizeof(unsigned) * k);
     j = m  = 0;
     b[m++] = a[j++];
     while (j < k)
@@ -460,12 +426,24 @@ void rleFrPoly(RLE* R, const double* xy, uint64_t k, uint64_t h, uint64_t w) {
     free(b);
 }
 
+/* Convert bounding boxes to encoded masks. */
+void rleFrBbox(RLE* R, const double* bb, unsigned long long h, unsigned long long w, unsigned long long n) {
+    unsigned long long i;
+    for (i = 0; i < n; i++) {
+        double xs = bb[4 * i + 0], xe = xs + bb[4 * i + 2];
+        double ys = bb[4 * i + 1], ye = ys + bb[4 * i + 3];
+        double xy[8] = { xs, ys, xs, ye, xe, ye, xe, ys };
+        rleFrPoly(R + i, xy, 4, h, w);
+    }
+}
+
+/* Get compressed string representation of encoded mask. */
 char* rleToString(const RLE* R) {
     /* Similar to LEB128 but using 6 bits/char and ascii chars 48-111. */
-    uint64_t i, m = R->m, p = 0;
-    long     x;
-    int      more;
-    char*    s = malloc(sizeof(char) * m * 6);
+    unsigned long long i, m = R->m, p = 0;
+    long               x;
+    int                more;
+    char*              s = malloc(sizeof(char) * m * 6);
     for (i = 0; i < m; i++) {
         x = (long) R->cnts[i];
         if (i > 2) x -= (long) R->cnts[i - 2];
@@ -483,13 +461,14 @@ char* rleToString(const RLE* R) {
     return s;
 }
 
-void rleFrString(RLE* R, char* s, uint64_t h, uint64_t w) {
-    uint64_t  m = 0, p = 0, k;
-    long      x;
-    int       more;
-    uint32_t* cnts;
+/* Convert from compressed string representation of encoded mask. */
+void rleFrString(RLE* R, char* s, unsigned long long h, unsigned long long w) {
+    unsigned long long m = 0, p = 0, k;
+    long               x;
+    int                more;
+    unsigned*          cnts;
     while (s[m]) m++;
-    cnts = malloc(sizeof(uint32_t) * m);
+    cnts = malloc(sizeof(unsigned) * m);
     m    = 0;
     while (s[p]) {
         x    = 0;
@@ -504,8 +483,14 @@ void rleFrString(RLE* R, char* s, uint64_t h, uint64_t w) {
             if (!more && (c & 0x10)) x |= -1 << 5 * k;
         }
         if (m > 2) x += (long) cnts[m - 2];
-        cnts[m++] = (uint32_t) x;
+        cnts[m++] = (unsigned) x;
     }
     rleInit(R, h, w, m, cnts);
     free(cnts);
+}
+
+int main(void) {
+    //
+    _putws(L"Hellooooooo! It's been a while since I last used C!");
+    return EXIT_SUCCESS;
 }
