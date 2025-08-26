0800d9465b5317c5f91935be159ddc9b8236e272
anoban <2016sb117@univ.jfn.ac.lk>
2025-08-26 14:44:48 +1000

    modified:   notebooks/phylogeny.ipynb 	modified:   rscripts/phylo.R

--- rscripts/phylo.R
+++ rscripts/phylo.R
@@ -1,13 +1,16 @@
-# install the package
-# devtools::install_github("jinyizju/U.PhyloMaker")
-# install.packages("ape")
-# UPhyloMaker is already installed
+library("ape")
+library("U.PhyloMaker")
+library("ggplot2")
+library("maps")
+library("phytools")
 
 genus_family_relations <- read.csv("../data/UPhyloMaker/plant_genus_list.csv", sep = ",")
 species_of_interest <- read.csv("../data/UPhyloMaker/fred_binom_genus.csv", sep = ",")
 megatree <- ape::read.tree("../data/UPhyloMaker/plant_megatree.tre")
 
 
-
+runtime <- Sys.time()
 phylogeny <- U.PhyloMaker::phylo.maker(sp.list = species_of_interest, tree = megatree, gen.list = genus_family_relations)
+runtime <- sys.time() - clock
+
 ape::write.tree(phy = phylogeny$phylo, file = "../data/UPhyloMaker/chapter03.tre")
