81321a2848d0d3d79d20aa7eac95f3e05fb4c587
anoban <2016sb117@univ.jfn.ac.lk>
2025-08-27 09:09:56 +1000

    modified:   notebooks/chapter 3.ipynb 	modified:   rscripts/phylo.R

--- rscripts/phylo.R
+++ rscripts/phylo.R
@@ -10,12 +10,15 @@ species_of_interest <- read.csv("../data/UPhyloMaker/fred_binom_genus.csv", sep 
 megatree <- ape::read.tree("../data/UPhyloMaker/plant_megatree.tre")
 
 
-runtime <- Sys.time()
-# this took forfuckingever
-phylogeny <- U.PhyloMaker::phylo.maker(sp.list = species_of_interest, tree = megatree, gen.list = genus_family_relations)
-runtime <- sys.time() - clock
+# runtime <- Sys.time()
+# this took forfuckingever ~ 3 minutes
+# phylogeny <- U.PhyloMaker::phylo.maker(sp.list = species_of_interest, tree = megatree, gen.list = genus_family_relations)
+runtime <- Sys.time() - runtime
 
 # serialize the new phylogenetic tree
-ape::write.tree(phy = phylogeny$phylo, file = "../data/UPhyloMaker/chapter03.tre")
+# ape::write.tree(phy = phylogeny$phylo, file = "../data/UPhyloMaker/chapter03.tre")
+# ggtree::ggtree(phylogeny$phylo, layout = "fan", open.angle = 120)
 
-ggtree::ggtree(phylogeny$phylo, layout = "fan", open.angle = 120)
+chap3 <- ape::read.tree("../data/UPhyloMaker/chapter03.tre")
+plot <- ggtree::ggtree(chap3, layout = "circular") + ggtree::geom_tiplab(size = 2)
+plot
