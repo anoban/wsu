5c8db3a7105941424f9ddc3169f78f3adc643a26
anoban <2016sb117@univ.jfn.ac.lk>
2025-08-26 14:24:59 +1000

    renamed:    notebooks/biopython.ipynb -> notebooks/phylogeny.ipynb 	deleted:    notebooks/phylomatic.ipynb 	modified:   rscripts/phylo.R

--- rscripts/phylo.R
+++ rscripts/phylo.R
@@ -1,1 +1,13 @@
-#
+# install the package
+# devtools::install_github("jinyizju/U.PhyloMaker")
+# install.packages("ape")
+# UPhyloMaker is already installed
+
+genus_family_relations <- read.csv("../data/UPhyloMaker/plant_genus_list.csv", sep = ",")
+species_of_interest <- read.csv("../data/UPhyloMaker/fred_binom_genus.csv", sep = ",")
+megatree <- ape::read.tree("../data/UPhyloMaker/plant_megatree.tre")
+
+
+
+phylogeny <- U.PhyloMaker::phylo.maker(sp.list = species_of_interest, tree = megatree, gen.list = genus_family_relations)
+ape::write.tree(phy = phylogeny$phylo, file = "../data/UPhyloMaker/chapter03.tre")
