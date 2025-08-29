87f656b9930279d895aead85423195e89d69b097
anoban <2016sb117@univ.jfn.ac.lk>
2025-08-29 13:18:01 +1000

    modified:   models/DeepLabV3Plus-Pytorch/datasets/cityscapes.py 	modified:   models/DeepLabV3Plus-Pytorch/datasets/utils.py 	modified:   models/DeepLabV3Plus-Pytorch/datasets/voc.py 	modified:   models/DeepLabV3Plus-Pytorch/predict.py

--- models/DeepLabV3Plus-Pytorch/datasets/utils.py
+++ models/DeepLabV3Plus-Pytorch/datasets/utils.py
@@ -1,7 +1,8 @@
+import errno
+import hashlib
 import os
 import os.path
-import hashlib
-import errno
+
 from tqdm import tqdm
 
 
@@ -21,9 +22,9 @@ def check_integrity(fpath, md5=None):
     if not os.path.isfile(fpath):
         return False
     md5o = hashlib.md5()
-    with open(fpath, 'rb') as f:
+    with open(fpath, "rb") as f:
         # read in 1MB chunks
-        for chunk in iter(lambda: f.read(1024 * 1024), b''):
+        for chunk in iter(lambda: f.read(1024 * 1024), b""):
             md5o.update(chunk)
     md5c = md5o.hexdigest()
     if md5c != md5:
@@ -63,23 +64,16 @@ def download_url(url, root, filename=None, md5=None):
 
     # downloads file
     if os.path.isfile(fpath) and check_integrity(fpath, md5):
-        print('Using downloaded and verified file: ' + fpath)
+        print("Using downloaded and verified file: " + fpath)
     else:
         try:
-            print('Downloading ' + url + ' to ' + fpath)
-            urllib.request.urlretrieve(
-                url, fpath,
-                reporthook=gen_bar_updater(tqdm(unit='B', unit_scale=True))
-            )
+            print("Downloading " + url + " to " + fpath)
+            urllib.request.urlretrieve(url, fpath, reporthook=gen_bar_updater(tqdm(unit="B", unit_scale=True)))
         except OSError:
-            if url[:5] == 'https':
-                url = url.replace('https:', 'http:')
-                print('Failed download. Trying https -> http instead.'
-                      ' Downloading ' + url + ' to ' + fpath)
-                urllib.request.urlretrieve(
-                    url, fpath,
-                    reporthook=gen_bar_updater(tqdm(unit='B', unit_scale=True))
-                )
+            if url[:5] == "https":
+                url = url.replace("https:", "http:")
+                print("Failed download. Trying https -> http instead. Downloading " + url + " to " + fpath)
+                urllib.request.urlretrieve(url, fpath, reporthook=gen_bar_updater(tqdm(unit="B", unit_scale=True)))
 
 
 def list_dir(root, prefix=False):
@@ -90,12 +84,7 @@ def list_dir(root, prefix=False):
             only returns the name of the directories found
     """
     root = os.path.expanduser(root)
-    directories = list(
-        filter(
-            lambda p: os.path.isdir(os.path.join(root, p)),
-            os.listdir(root)
-        )
-    )
+    directories = list(filter(lambda p: os.path.isdir(os.path.join(root, p)), os.listdir(root)))
 
     if prefix is True:
         directories = [os.path.join(root, d) for d in directories]
@@ -113,12 +102,7 @@ def list_files(root, suffix, prefix=False):
             only returns the name of the files found
     """
     root = os.path.expanduser(root)
-    files = list(
-        filter(
-            lambda p: os.path.isfile(os.path.join(root, p)) and p.endswith(suffix),
-            os.listdir(root)
-        )
-    )
+    files = list(filter(lambda p: os.path.isfile(os.path.join(root, p)) and p.endswith(suffix), os.listdir(root)))
 
     if prefix is True:
         files = [os.path.join(root, d) for d in files]
