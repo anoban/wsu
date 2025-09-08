9acc0ce7c4e667cea979b77b94c31d3ad9a7649c
anoban <2016sb117@univ.jfn.ac.lk>
2025-09-09 09:29:29 +1000

    modified:   data/references.txt 	modified:   models/Legume-Root-Analysis/Average_diameter.py 	modified:   models/Legume-Root-Analysis/Calibration.py 	modified:   models/Legume-Root-Analysis/SA_measured.py 	modified:   models/Legume-Root-Analysis/Surface_area_estimated.py 	modified:   models/Legume-Root-Analysis/TRL.py 	modified:   notebooks/references.ipynb

--- models/Legume-Root-Analysis/Average_diameter.py
+++ models/Legume-Root-Analysis/Average_diameter.py
@@ -1,14 +1,13 @@
-
 import cv2
 
-image = cv2.imread("D:/1.tiff") 
-image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
-image = cv2.bitwise_not(image) 
-blur = cv2.medianBlur(image,5) 
-ret3, binary_image = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU) 
-ret3, image = cv2.threshold(blur, 0 ,255, cv2.THRESH_BINARY+cv2.THRESH_TRIANGLE) 
+image = cv2.imread("D:/1.tiff")
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+image = cv2.bitwise_not(image)
+blur = cv2.medianBlur(image, 5)
+ret3, binary_image = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+ret3, image = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_TRIANGLE)
 skeleton_image = cv2.ximgproc.thinning(image)
-num_components, labels, stats, centroids = cv2.connectedComponentsWithStats(skeleton_image) 
+num_components, labels, stats, centroids = cv2.connectedComponentsWithStats(skeleton_image)
 length = stats[:, cv2.CC_STAT_AREA]
 length = list(sorted(length))
 assert len(length) >= 2
@@ -17,7 +16,7 @@ noise = 250
 for i, length in enumerate(length[:-1]):
     if length > noise:
         total_length += length
-        TRL = total_length 
+        TRL = total_length
 pixel_size_cm = 0.0063
 distance_transform = cv2.distanceTransform(binary_image, cv2.DIST_L2, 5)
 diameter_sum = 0
