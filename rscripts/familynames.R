109e5bf7025219fe7b095e5e27b3fd95fc3025e0
anoban <2016sb117@univ.jfn.ac.lk>
2025-09-16 10:00:56 +1000

    new file:   data/chapter2/plantlookup_serialized_from_r.csv 	modified:   rscripts/familynames.R

--- rscripts/familynames.R
+++ rscripts/familynames.R
@@ -1,9 +1,12 @@
-# install.packages("devtools")
-# devtools::install_github("ropenscilabs/datastorr")
-# devtools::install_github("wcornwell/taxonlookup")
+if (!require("devtools")) install.packages("devtools")
+if (!require("datastorr")) devtools::install_github("ropenscilabs/datastorr")
+if (!require("taxonlookup")) devtools::install_github("wcornwell/taxonlookup")
 
 library(taxonlookup)
 
 lookup_table(c("Pinus ponderosa", "Quercus agrifolia"), by_species = TRUE)
 lookup_table(c("Cynodon dactylon", "Azadirachta indica"), by_species = TRUE)
-write.csv(data.frame(plant_lookup()), fileEncoding = "latin1", file = "./plantlookup.csv", quote = FALSE, row.names = FALSE)
+
+# this is more elegant than the version downloadable from github :/
+write.csv(data.frame(plant_lookup()), fileEncoding = "latin1", file = "../data/chapter2/plantlookup_serialized_from_r.csv",
+          quote = FALSE, row.names = FALSE)
