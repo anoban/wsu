4541e679cbf567381e02000bb029d0537b25817d
anoban <2016sb117@univ.jfn.ac.lk>
2025-09-16 14:20:40 +1000

    modified:   models/Legume-Root-Analysis/skeleton.py

--- models/Legume-Root-Analysis/skeleton.py
+++ models/Legume-Root-Analysis/skeleton.py
@@ -13,11 +13,11 @@ class skeleton(object):
         try:
             with open(file=filepath, mode="rb") as fp:
                 self.pixels = np.array(Image.open(fp=fp), dtype=np.float32)
-                self.width = 0
-                self.height = 0
-                self.colour_channels = 3
+                self.width = 0  # TODO
+                self.height = 0  # TODO
+                self.colour_channels = 3  # TODO
         except IOError as ioexcept:
-            raise RuntimeError("") from ioexcept
+            raise RuntimeError("") from ioexcept  # TODO
 
     def skeletonize(self, inplace: bool = False) -> Self | None:
         """
