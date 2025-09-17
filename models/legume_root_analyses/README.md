8d3262b5184478e4b1571e84b8311b507a0d3780
anoban <2016sb117@univ.jfn.ac.lk>
2025-09-17 14:40:17 +1000

    modified:   models/legume_root_analyses/README.md 	modified:   models/legume_root_analyses/diameter.py

--- models/legume_root_analyses/README.md
+++ models/legume_root_analyses/README.md
@@ -1,5 +1,5 @@
-Ghimire, A. et al. (2025) “Python algorithm package for automated Estimation of major legume root traits using two dimensional images”
-Scientific Reports, 15(1). Available at: https://doi.org/10.1038/s41598-025-91993-y.
-The images used in the analysis can be downloaded from: https://drive.google.com/file/d/17Sh1-uqEqRoZzXxD8e6AMj4tb5_q9qkI/view?usp=sharing
+_This directory is a modified clone of https://github.com/AG9843/Legume-Root-Analysis, which contains the original codebase of the below publication._
 
-__This directory is a modified clone of https://github.com/AG9843/Legume-Root-Analysis, which contains the original codebase of the publication.__
+Ghimire, A. et al. (2025) “Python algorithm package for automated Estimation of major legume root traits using two dimensional images” Scientific Reports, 15(1). Available at: https://doi.org/10.1038/s41598-025-91993-y.
+
+The images used in the original analysis can be downloaded from: https://drive.google.com/file/d/17Sh1-uqEqRoZzXxD8e6AMj4tb5_q9qkI/view?usp=sharing
--- models/legume_root_analyses/diameter.py
+++ models/legume_root_analyses/diameter.py
@@ -1,4 +1,7 @@
-import cv2
+import numpy as np
+from numpy.typing import NDArray
+
+from .params import PIXEL_SIZE_CENTIMETERS
 
 # We'll port this to use scikitimage instead of the bloated OpenCV
 
@@ -13,27 +16,35 @@ num_components, labels, stats, centroids = cv2.connectedComponentsWithStats(skel
 length = stats[:, cv2.CC_STAT_AREA]
 length = list(sorted(length))
 assert len(length) >= 2
+
 total_length = 0
 noise = 250
 for i, length in enumerate(length[:-1]):
     if length > noise:
         total_length += length
         TRL = total_length
-pixel_size_cm = 0.0063
+
 distance_transform = cv2.distanceTransform(binary_image, cv2.DIST_L2, 5)
 diameter_sum = 0
 
 for y in range(skeleton_image.shape[0]):
     for x in range(skeleton_image.shape[1]):
         if skeleton_image[y, x] > 0:  # Check if the pixel is part of the skeleton
-            radius = distance_transform[y, x] * pixel_size_cm
+            radius = distance_transform[y, x] * PIXEL_SIZE_CENTIMETERS
             diameter = 2 * radius
             diameter_sum += diameter
 average_diameter = diameter_sum / TRL
+
 print("Average Diameter (cm):", average_diameter)
 
 
-def __average_diameter() -> float:
+def __average_diameter(skeletonized_image: NDArray[np.floating | np.integer]) -> float:
     """ """
 
-    return 0.0000
+    for y in range(skeleton_image.shape[0]):
+        for x in range(skeleton_image.shape[1]):
+            if skeleton_image[y, x] > 0:  # Check if the pixel is part of the skeleton
+                radius = distance_transform[y, x] * PIXEL_SIZE_CENTIMETERS
+                diameter = 2 * radius
+                diameter_sum += diameter
+    return diameter_sum / TRL
