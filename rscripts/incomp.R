4950bf4ad4673983a6042594aa11d1a773026dbd
anoban <2016sb117@univ.jfn.ac.lk>
2025-10-14 09:57:53 +1100

    modified:   rscripts/incomp.R

--- rscripts/incomp.R
+++ rscripts/incomp.R
@@ -1,4 +1,7 @@
-# incomplete block designs
+# incomplete block designs & power analyses
 if (!require("ibd")) {
     install.packages("ibd")
+}
+if (!require("pwr")) {
+    install.packages("pwr")
 }
