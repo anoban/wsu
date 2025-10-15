e54b5c5e8a658716f6ff3d5c648a5c5379e5670d
anoban <2016sb117@univ.jfn.ac.lk>
2025-10-16 10:08:36 +1100

    modified:   notebooks/chapter_02/photosynthetic_pathways.ipynb 	new file:   perf/power.cpp 	new file:   perf/power.py

---
+++ perf/power.cpp
@@ -0,0 +1,36 @@
+// statistical functions that need to be handrolled or stolen from GSL :)
+// t ppf
+// nct sf
+// f ppf
+// ncf sf
+// norm cdf
+// chi2 ppf
+// ncchi2 sf
+// brenth
+
+#include <cstdlib>
+
+namespace stats {
+
+    // the nc prefix stands for non central variants of the distributions
+
+    namespace t { }
+
+    namespace nct { }
+
+    namespace f { }
+
+    namespace ncf { }
+
+    namespace norm { }
+
+    namespace chisq { }
+
+    namespace ncchisq { }
+
+} // namespace stats
+
+int wmain() {
+    //
+    return EXIT_SUCCESS;
+}
