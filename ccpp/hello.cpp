a2b32a5434cf8f1446b5a2d035be5b2bb05de340
anoban <2016sb117@univ.jfn.ac.lk>
2025-10-21 08:19:19 +1100

    modified:   ccpp/hello.cpp

--- ccpp/hello.cpp
+++ ccpp/hello.cpp
@@ -4,7 +4,7 @@
 static inline void __cdecl say_goodbye(void) { ::_putws(L"I'm about to head out, good bye!"); }
 
 int wmain(_In_ const int argc, _In_reads_(argc) const wchar_t* argv[]) {
-    if (::atexit(::say_goodbye)) { // returns 0 if succeeds
+    if (::atexit(::say_goodbye) /* atexit() returns 0 if succeeds */) { // NOLINT(readability-implicit-bool-conversion)
         ::fputws(L"atexit() registration failed!", stderr);
         return EXIT_FAILURE;
     }
