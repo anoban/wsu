858cb0a3dab2d6a8ffe4d2d619c44ff32c6467b9
anoban <2016sb117@univ.jfn.ac.lk>
2025-10-30 13:58:04 +1100

    modified:   notebooks/chapter_01/coc.ipynb 	modified:   notebooks/chapter_01/image-annotations.ipynb 	modified:   notebooks/chapter_03/environment.ipynb 	modified:   rscripts/jeff/eucfacePower_v3.R 	new file:   rscripts/jeff/power.py

--- rscripts/jeff/eucfacePower_v3.R
+++ rscripts/jeff/eucfacePower_v3.R
@@ -7,8 +7,7 @@ tryCatch.converg <- function(expr) {
     converged <- T
 
     w.handler <- function(w) {
-        converged <<- F
-        # <<- operator targets the parent scope, akin to global variable_name in python!
+        converged <<- F # <<- operator targets the parent scope, akin to global variable_name in python!
         invokeRestart("muffleWarning")
     }
 
