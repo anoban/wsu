ac2841eeb86ff91488579e90d9526f6c453c49e2
anoban <2016sb117@univ.jfn.ac.lk>
2025-12-01 12:55:49 +1100

    new file:   correlated.ipynb 	modified:   correlatedevolution.R

--- R/correlatedevolution.R
+++ R/correlatedevolution.R
@@ -1,13 +1,8 @@
-library("ape")
-library("phytools")
-library("nlme")
-library("corHMM")
-library("geiger")
-library("mkcor")
-library("OUwie")
+
 
 # make sure the OUwie version you have is recent 2.16 NOT the old ones because they do not have the OUwie::hOUwie() function
-# to get the recent version, clone the github repo and use R CMD build --no-build-vignettes followed by INSTALL.
+# CRAN CDN delivers 2.10 by default
+# to get the recent version, clone the github repo and use R CMD build --no-build-vignettes followed by R CMD INSTALL
 # https://thej022214.github.io/OUwie/reference/hOUwie.html
 
 data <- read.csv("../data/chapter2/FREDv3subset/FRED_subset_collab_states_n_species_avg_traits.csv", row.names = "binominal")
