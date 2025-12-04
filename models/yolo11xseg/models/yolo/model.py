d4e94c72ada4b2344ce3552e0c31f7e660063690
anoban <2016sb117@univ.jfn.ac.lk>
2025-12-05 10:00:50 +1100

    modified:   R/correlated.ipynb 	modified:   models/yolo11xseg/models/yolo/model.py

--- models/yolo11xseg/models/yolo/model.py
+++ models/yolo11xseg/models/yolo/model.py
@@ -37,7 +37,7 @@ class YOLO(Model):
         >>> model = YOLO("yolo11n.yaml")
     """
 
-    def __init__(self, model: str | Path = "yolo11n.pt", task: str | None = None, verbose: bool = False):
+    def __init__(self, model: str | Path, task: str | None = None, verbose: bool = False):
         """Initialize a YOLO model.
 
         This constructor initializes a YOLO model, automatically switching to specialized model types (YOLOWorld or
@@ -55,23 +55,9 @@ class YOLO(Model):
             >>> model = YOLO("yolo11n-seg.pt")  # load a pretrained YOLO11n segmentation model
         """
         path = Path(model if isinstance(model, (str, Path)) else "")
-        if "-world" in path.stem and path.suffix in {".pt", ".yaml", ".yml"}:  # if YOLOWorld PyTorch model
-            new_instance = YOLOWorld(path, verbose=verbose)
-            self.__class__ = type(new_instance)
-            self.__dict__ = new_instance.__dict__
-        elif "yoloe" in path.stem and path.suffix in {".pt", ".yaml", ".yml"}:  # if YOLOE PyTorch model
-            new_instance = YOLOE(path, task=task, verbose=verbose)
-            self.__class__ = type(new_instance)
-            self.__dict__ = new_instance.__dict__
-        else:
-            # Continue with default YOLO initialization
-            super().__init__(model=model, task=task, verbose=verbose)
-            if hasattr(self.model, "model") and "RTDETR" in self.model.model[-1]._get_name():  # if RTDETR head
-                from ultralytics import RTDETR
 
-                new_instance = RTDETR(self)
-                self.__class__ = type(new_instance)
-                self.__dict__ = new_instance.__dict__
+        # Continue with default YOLO initialization
+        super().__init__(model=model, task=task, verbose=verbose)
 
     @property
     def task_map(self) -> dict[str, dict[str, Any]]:
