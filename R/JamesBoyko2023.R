a9c5e6e16b33e2c282f5ff57a45927a945e4c53d
anoban <2016sb117@univ.jfn.ac.lk>
2025-12-05 14:27:54 +1100

    new file:   R/JamesBoyko2023.R 	modified:   R/correlated.ipynb 	renamed:    notebooks/chapter_01/sa1b/sa_1822.json -> notebooks/chapter_01/SA1B/sa_1822.json 	renamed:    notebooks/chapter_01/sa1b/sa_350.json -> notebooks/chapter_01/SA1B/sa_350.json 	renamed:    notebooks/chapter_01/sa1b/sa_5857.json -> notebooks/chapter_01/SA1B/sa_5857.json 	renamed:    notebooks/chapter_01/sa1b/sa_5906.json -> notebooks/chapter_01/SA1B/sa_5906.json 	renamed:    notebooks/chapter_01/sa1b/sa_6167.json -> notebooks/chapter_01/SA1B/sa_6167.json 	renamed:    notebooks/chapter_01/sa1b/sa_685.json -> notebooks/chapter_01/SA1B/sa_685.json 	renamed:    notebooks/chapter_01/sa1b/sa_763.json -> notebooks/chapter_01/SA1B/sa_763.json 	renamed:    notebooks/chapter_01/sa1b/sa_901.json -> notebooks/chapter_01/SA1B/sa_901.json 	renamed:    notebooks/chapter_01/sa1b/sa_9480.json -> notebooks/chapter_01/SA1B/sa_9480.json

---
+++ R/JamesBoyko2023.R
@@ -0,0 +1,5 @@
+load("./2020_houwie/empirical_fit/FitSD=CID+_OUM.Rsave")
+load("./2020_houwie/empirical_fit/FitSD=CID_OU1.Rsave")
+load("./2020_houwie/empirical_fit/FitSD=CD_OUM.Rsave")
+load("./2020_houwie/empirical_fit/FitSD=HYB_OUM.Rsave")
+
