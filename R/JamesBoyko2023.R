f113270208776c21242c69312b168fa55d447d15
anoban <2016sb117@univ.jfn.ac.lk>
2025-12-06 20:40:59 +1100

    modified:   R/JamesBoyko2023.R 	modified:   R/correlated.ipynb 	new file:   R/googlecolab_correlated.ipynb 	deleted:    code/R/googlecolab_correlated.ipynb

--- R/JamesBoyko2023.R
+++ R/JamesBoyko2023.R
@@ -1,12 +1,13 @@
 for (model in list.files(path = "./thirdparty/2020_houwie/empirical_fit")){
     load(file = paste0("./thirdparty/2020_houwie/empirical_fit", "/", model))
-    print()
+    print(paste("model =", model,
+                "rate.cat =", fit$rate.cat,
+                "discrete_model =", fit$discrete_model,
+                "continuous_model =", fit$continuous_model,
+                "nSim =", fit$nSim,
+                "null.model =", fit$null.model
+                ))
 }
 
 
-
-load("./2020_houwie/empirical_fit/FitSD=CID+_OUM.Rsave")
-load("./2020_houwie/empirical_fit/FitSD=CID_OU1.Rsave")
-load("./2020_houwie/empirical_fit/FitSD=CD_OUM.Rsave")
-load("./2020_houwie/empirical_fit/FitSD=HYB_OUM.Rsave")
 
