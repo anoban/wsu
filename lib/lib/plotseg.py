1d3bf81f3c1834db9a192e8aaaaf4103d68c6878
anoban <2016sb117@univ.jfn.ac.lk>
2025-12-09 09:02:31 +1100

    new file:   lib/lib/plotseg.py 	deleted:    models/sam2/download_ckpts.sh

---
+++ lib/lib/plotseg.py
@@ -0,0 +1,28 @@
+import numpy as np
+from matplotlib.axes import Axes
+from numpy.typing import NDArray
+
+
+def plot_segmentations(ax: Axes, masks: NDArray[np.integer], height: int, width: int, alpha: float = 0.35, borders: bool = True) -> Axes:
+    """
+    overlay segmentation results of a segmentation model on an image (or empty axes)
+    the parameter `masks` is expected to be a 3 dimensional numpy array of shape (N, H, W) where N is the number of binary masks,
+    H is expected to match the height of the image and W is expected to match the width of the image.
+    each mask in the masks object is expected to be transformable to a boolean matrix, to subset the pixels that need colouring during plotting.
+    alpha is transparency applied to the mask when it's overlaid on the image, for better transparency, use higher alpha values.
+    borders is to specify whether a border needs to be drawn, outlining each segmentation result, instead of using colours alone to delineate the objects.
+    """
+
+    # TODO implement the border drawing functionality
+
+    assert masks.shape[1] == height and masks.shape[2] == width, "dimensions of the masks do not match the params height and width"
+
+    rgba = np.ones((height, width, 4))
+    for mask in masks:
+        rgba[:, :, 3] = 0  # set all the alpha channel values to 0
+        # use the boolean mask to cherry pick the elements where the mask applies
+        rgba[mask.astype(np.bool), :] = np.array(
+            [*np.random.random(3), alpha]
+        )  # and update the colour (RGB) values with the specified alpha value
+        ax.imshow(rgba)  # type: ignore
+    return ax
