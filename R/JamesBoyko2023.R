9a080fd8bce8da76893e7465955c1aabba4ea3c1
anoban <2016sb117@univ.jfn.ac.lk>
2025-12-11 13:13:55 +1100

    modified:   R/JamesBoyko2023.R

--- R/JamesBoyko2023.R
+++ R/JamesBoyko2023.R
@@ -20,14 +20,14 @@ suppressPackageStartupMessages({
 # pick family Ericaceae
 
 # we don't want the other unnecessary columns
-fruits <- read.csv("../R/thirdparty/seed_dispersal/trait_data/Ericaceae_trait_data.csv")[, c("Species", "Fruit_type")]
+fruits <- read.csv("../R/thirdparty/seed_dispersal/trait_dataa/Ericaceae_trait_dataa.csv")[, c("Species", "Fruit_type")]
 fruits <- fruits[fruits$Fruit_type == "Fleshy" | fruits$Fruit_type == "Dry", ] # drop the rows that do not have actual data for life form
 # Fruit_type is a binary discrete categorical trait with values Fleshy and Dry
 fruits$Fruit_type |> table()
 dim(fruits ) # 444 species
 
 # continuous traits
-climate <- read.csv("../R/thirdparty/seed_dispersal/trait_data/Ericaceae_niche.csv")
+climate <- read.csv("../R/thirdparty/seed_dispersal/trait_dataa/Ericaceae_niche.csv")
 # looks like this too has the fruit type info embedded
 climate$Fruit_type |> table() # yup
 
@@ -37,3 +37,29 @@ tree <- ape::read.tree("../R/thirdparty/seed_dispersal/trees/Ericaceae_Schwery_e
 ape::is.binary(tree) # TRUE :)
 
 # look up https://github.com/jboyko/2020_houwie/blob/master/04_empirical-seed-dispersal.R for reference
+
+# drop the species in the dataaset if they are not in the phylogenetic tree
+species <- intersect(tree$tip.label, climate$species) # 309 species
+indices <- match(species, climate$species)
+data <- climate[indices, ]
+
+# make sure that we only have species for which there's data in the phylogeny
+stopifnot(length(intersect(data$species, tree$tip.label)) == length(indices))
+
+# drop the unnecessary species from the phylogeny
+species_to_drop_from_tree <- setdiff(tree$tip.label, species) # order of args matters here!!!
+pruned_tree <- ape::drop.tip(phy = tree, tip = species_to_drop_from_tree)
+
+# visualize the pruned phylogeny
+Tmax <- max(ape::branching.times(pruned_tree))
+plot(pruned_tree, show.tip.label = FALSE, x.lim = c(0, Tmax + 0.2 * Tmax))
+start <- Tmax + (0.005 * Tmax)
+data.prop <- (data[,3] - min(data[,3]))/max(data[,3] - min(data[,3]))
+xadd <- Tmax * 0.2
+jitter <- 0.1 * xadd
+cols = c("brown", "purple")
+for(i in 1:length(data.prop)){
+    lines(list(x = c(start, start + (data.prop[i] * xadd)), y = c(i,i)),
+          col = cols[ifelse(data$reg[i] == "Dry", 1, 2)],
+          lwd = 1)
+}
