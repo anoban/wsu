39230ec68ace714ca3ee0363b0a6a13f3aa55c95
anoban <2016sb117@univ.jfn.ac.lk>
2026-02-04 10:22:24 +1100

    modified:   R/parhouwie.c deleted:    data/chapter2/FREDv3subset/FRED_subset.csv deleted:    data/chapter2/FREDv3subset/FRED_subset_collab_categorical.csv deleted:    data/chapter2/FREDv3subset/FRED_subset_collab_unique_taxa.csv deleted:    data/chapter2/FREDv3subset/FRED_subset_ord1_cont_RES_traits.csv deleted:    data/chapter2/FREDv3subset/FRED_subset_ord1_sp_avrgd_cont_RES_traits.csv deleted:    data/chapter2/FREDv3subset/collab_1005sp_states_n_sp_avgd_RD_n_SRL.csv new file:   data/chapter2/FREDv3subset/collab_ord1_SRL_RD_species_avgd.csv deleted:    data/chapter2/FREDv3subset/collab_ord1_all_records_SRL_RD.csv new file:   data/chapter2/FREDv3subset/collab_ord1_log_SRL_RD_species_avgd.csv deleted:    data/chapter2/FREDv3subset/collab_ord1_species_avgs_SRL_RD.csv modified:   data/chapter2/FREDv3subset/collab_rdlteq1_rd1ornan_RD_SRL_species_avgd.csv renamed:    data/chapter2/FREDv3subset/collab_log_RD_SRL_1005species_avgd.csv -> data/chapter2/FREDv3subset/collab_rdlteq1_rd1ornan_log_RD_SRL_species_avgd.csv deleted:    data/chapter2/FREDv3subset/finalized_states_395_species.csv deleted:    data/chapter2/FREDv3subset/fred_binom_genus.csv deleted:    data/chapter2/FREDv3subset/fred_gen_fam.csv deleted:    data/chapter2/FREDv3subset/species_clean_states.csv deleted:    data/chapter2/FREDv3subset/species_missing_and_conflciting_states.csv deleted:    data/chapter2/FREDv3subset/state_conflicts.xlsx deleted:    data/chapter2/FREDv3subset/state_conflicts_resolved.csv deleted:    data/chapter2/FREDv3subset/unified_states_to_fill_FRED_TRY_Maherali_GRoot.csv modified:   notebooks/chapter_02/subsetting.ipynb

--- R/parhouwie.c
+++ R/parhouwie.c
@@ -1,3 +1,4 @@
+// clang .\parhouwie.c -Wall -Wextra -static -march=native -DNDEBUG -O3 -std=c23
 // launch the R interpretor in parallel for the hOUwie model fits
 
 // clang-format off
@@ -21,22 +22,20 @@ static const wchar_t* const LIBRARY_LOADING__AND_SANITATION = L"suppressPackageS
                                                               "stopifnot(packageVersion(\"corHMM\") == \"2.8\")";
 
 // template string for leading in the phylogeny data and the trait data
-static const wchar_t* const DATA_LOADING_TEMPLATE =
-    L"phylogeny <- ape::multi2di(ape::read.tree(file = \"../../data/chapter2/uphylomaker/%s\"))" // assume that the directories won't need changing
-    "trait_data <- read.csv(\"../../data/chapter2/FREDv3subset/%s\", stringsAsFactors = TRUE)"; // same here
+static const wchar_t* const DATA_LOADING_TEMPLATE = L"phylogeny <- ape::multi2di(ape::read.tree(file = \"%s\"))"
+                                                    "trait_data <- read.csv(\"%s\", stringsAsFactors = TRUE)";
 
-typedef enum DISCRETE_MODELS { ER, SYM, ARD } DISCRETE_MODELS; // DISCRETE MODELS
+static const wchar_t* const DISCRETE_MODELS[] = { L"OUM", L"OUMA", L"OUMV", L"OUMVA", nullptr };
 
-#define ER  L"ER"  // all rates are identical
-#define SYM L"SYM" // symmetrically identical rates
-#define ARD L"ARD" // all rates are allowed to be different (asymmetrically)
+typedef enum DISCRETE_MODELS_INDICES {
+    ER,  // all rates are identical
+    SYM, // symmetrically identical rates
+    ARD  // all rates are allowed to be different (asymmetrically)
+} DISCRETE_MODELS_INDICES;
 
-typedef enum CONTINUOUS_MODELS { OUM, OUMA, OUMV, OUMVA } CONTINUOUS_MODELS; // CONTINUOUS MODELS
+static const wchar_t* const CONTINUOUS_MODELS[] = { L"OUM", L"OUMA", L"OUMV", L"OUMVA", nullptr };
 
-#define OUM   L"OUM"   //
-#define OUMA  L"OUMA"  //
-#define OUMV  L"OUMV"  //
-#define OUMVA L"OUMVA" //
+typedef enum CONTINUOUS_MODELS_INDICES { OUM, OUMA, OUMV, OUMVA } CONTINUOUS_MODELS_INDICES;
 
 static inline bool __stdcall houwie() {
     // template string for fitting an hOUwie model, with customizable parameters
