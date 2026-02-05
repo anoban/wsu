bb30603991793c51ac13d6e5a1462c2b57308af2
anoban <2016sb117@univ.jfn.ac.lk>
2026-02-05 14:24:20 +1100

    modified:   R/parhOUwie.ipynb 	modified:   R/parhouwie.cpp

--- R/parhouwie.cpp
+++ R/parhouwie.cpp
@@ -15,118 +15,124 @@
 
 #include <cstdio>
 #include <cstdlib>
+#include <format>
 #include <string>
 
 // NOLINTBEGIN(cppcoreguidelines-pro-type-vararg,modernize-avoid-c-arrays)
 
-// get the string representation of a _WIN32 error code
-static inline const wchar_t* __stdcall error_code_to_string( // NOLINT(readability-redundant-inline-specifier)
-    _In_ const unsigned long errcode
-) noexcept {
-    static constexpr unsigned long long ERROR_MSG_BUFFSIZE { 512 }; // length of the error message buffer in number of wchar_t s
-    static wchar_t                      errmsgbuffer[ERROR_MSG_BUFFSIZE] = { 0 }; // needs to be in static memory
-    // without this the previously written buffer can get partially overwritten and returned in subsequent function invocations
-    ::memset(errmsgbuffer, 0, sizeof(errmsgbuffer));
+namespace utils {
 
-    unsigned long nbyteswritten = ::FormatMessageW(
-        FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, nullptr, errcode, 0, errmsgbuffer, ERROR_MSG_BUFFSIZE, nullptr
-    );
+    // get the string representation of a _WIN32 error code
+    static inline const wchar_t* __stdcall error_code_to_string( // NOLINT(readability-redundant-inline-specifier)
+        _In_ const unsigned long errcode
+    ) noexcept {
+        static constexpr unsigned long long ERROR_MSG_BUFFSIZE { 512 }; // length of the error message buffer in number of wchar_t s
+        static wchar_t                      errmsgbuffer[ERROR_MSG_BUFFSIZE] = { 0 }; // needs to be in static memory
+        // without this the previously written buffer can get partially overwritten and returned in subsequent function invocations
+        ::memset(errmsgbuffer, 0, sizeof(errmsgbuffer));
 
-    if (!nbyteswritten) { // will be 0 if the call above to FormatMessageW failed; if that, the error string is not found in the system, try Ntdsbmsg.dll
-        HINSTANCE handle_ntdsbmsg = ::LoadLibraryW(L"Ntdsbmsg.dll");
-        if (!handle_ntdsbmsg) { // will be NULL if the DLL failed to load
-            ::fputws(L"Failed to load Ntdsbmsg.dll", stderr);
-            return errmsgbuffer; // must be an ampty buffer here
-        }
-
-        nbyteswritten = ::FormatMessageW(
-            FORMAT_MESSAGE_FROM_HMODULE | FORMAT_MESSAGE_IGNORE_INSERTS,
-            handle_ntdsbmsg,
-            errcode,
-            0,
-            errmsgbuffer,
-            ERROR_MSG_BUFFSIZE,
-            nullptr
+        unsigned long nbyteswritten = ::FormatMessageW(
+            FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, nullptr, errcode, 0, errmsgbuffer, ERROR_MSG_BUFFSIZE, nullptr
         );
-        ::FreeLibrary(handle_ntdsbmsg); // detach the DLL from the process
+
+        if (!nbyteswritten) { // will be 0 if the call above to FormatMessageW failed; if that, the error string is not found in the system, try Ntdsbmsg.dll
+            HINSTANCE handle_ntdsbmsg = ::LoadLibraryW(L"Ntdsbmsg.dll");
+            if (!handle_ntdsbmsg) { // will be NULL if the DLL failed to load
+                ::fputws(L"Failed to load Ntdsbmsg.dll", stderr);
+                return errmsgbuffer; // must be an ampty buffer here
+            }
+
+            nbyteswritten = ::FormatMessageW(
+                FORMAT_MESSAGE_FROM_HMODULE | FORMAT_MESSAGE_IGNORE_INSERTS,
+                handle_ntdsbmsg,
+                errcode,
+                0,
+                errmsgbuffer,
+                ERROR_MSG_BUFFSIZE,
+                nullptr
+            );
+            ::FreeLibrary(handle_ntdsbmsg); // detach the DLL from the process
+        }
+        return errmsgbuffer;
     }
-    return errmsgbuffer;
+
+} // namespace utils
+
+namespace houwie {
+
+    enum class DISCRETE_MODELS : unsigned char {
+        ER,  // all rates are identical
+        SYM, // symmetrically identical rates
+        ARD  // all rates are allowed to be different (asymmetrically)
+    };
+
+    enum class CONTINUOUS_MODELS : unsigned char {
+        OUM,
+        OUMA [[deprecated("OUwie currently recommends not using the variable alpha models for continuous trait evolution")]],
+        OUMV,
+        OUMVA [[deprecated("OUwie currently recommends not using the variable alpha models for continuous trait evolution")]]
+    };
+
+    [[nodiscard]] static inline std::wstring __stdcall generate_rscript( // NOLINT(readability-redundant-inline-specifier)
+        const std::wstring&       phylogeny,
+        const std::wstring&       traitdata,
+        const DISCRETE_MODELS&    discrete_model,
+        const CONTINUOUS_MODELS&  continuous_model,
+        const std::wstring&       savedir,
+        const std::wstring&       conttrait,
+        const std::wstring&       disctrait,
+        const std::wstring&       suffix,
+        const bool&               null_model,
+        const unsigned&           rate_cat,
+        const unsigned long long& nsims = 30
+    ) noexcept {
+        static constexpr wchar_t TEMPLATE_SCRIPT[] {
+            L"suppressPackageStartupMessages({"
+            L"    library('ape')"
+            L"    library('corHMM')"
+            L"    library('OUwie')"
+            L"})"
+            L"phylogeny <- ape::read.tree(\"{}\")"
+            L"data <- read.csv(\"{}\")"
+            L"stopifnot(all(phylogeny$tip.label == data$binominal))"
+            L"model <- OUwie::hOUwie(phy = phylogeny, data = data, rate.cat = {}, discrete_model = \"{}\", continuous_model = \"{}\", nSim = {}, null.model = {})"
+            L"saveRDS(object = model, file = \"{}\")"
+        };
+
+        return std::vformat(TEMPLATE_SCRIPT, std::make_wformat_args());
+    }
 }
 
 // typically, each R process (inside Jupyter) only takes up about ~9% of the CPU, so this could absolutely benefit from paralellization
 // wait 5 seconds between launching new processes, so we don't run into (possible???) file I/O issues inside the R instances
 
-// this is a constant string for loading in the necessary libraries and checking their versions
-static const wchar_t* const LIBRARY_LOADING__AND_SANITATION = L"suppressPackageStartupMessages({"
-                                                              "    library(\"ape\")"
-                                                              "    library(\"corHMM\")"
-                                                              "    library(\"OUwie\")"
-                                                              "})"
-                                                              "stopifnot(packageVersion(\"OUwie\") == \"2.16\")"
-                                                              "stopifnot(packageVersion(\"corHMM\") == \"2.8\")";
-
-// template string for leading in the phylogeny data and the trait data
-static const wchar_t* const DATA_LOADING_TEMPLATE = L"phylogeny <- ape::multi2di(ape::read.tree(file = \"%s\"))"
-                                                    "trait_data <- read.csv(\"%s\", stringsAsFactors = TRUE)";
-
-enum class DISCRETE_MODELS : unsigned char {
-    ER,  // all rates are identical
-    SYM, // symmetrically identical rates
-    ARD  // all rates are allowed to be different (asymmetrically)
-};
-
-enum class CONTINUOUS_MODELS : unsigned char {
-    OUM,
-    OUMA [[deprecated("OUwie currently recommends not using the variable alpha models for continuous trait evolution")]],
-    OUMV,
-    OUMVA [[deprecated("OUwie currently recommends not using the variable alpha models for continuous trait evolution")]]
-};
-
 static wchar_t DUMMY_R_COMMANDLINE[512] {
     // the readline() at the end will keep the console window open until a user input is provided
-    L"C:/R-4.5.2/bin/Rscript.exe --no-save -e 'write.csv(installed.packages()[, c('Package', 'Version')], file = paste0('./packages_', sample(1:100, 1), '.csv'));readline();'"
+    L"C:/R-4.5.2/bin/Rscript.exe --no-save -e \"write.csv(installed.packages()[, c('Package', 'Version')], file = paste0('./rpacks', sample(1:100, 1), '.csv'))\""
 };
+
+// the issue is that when the R interpreter gets called, the expression gets passed with all the quotes stripped away - leads to syntax errors
+// figure out why the quotes get stripped away and how to preserve them when they are loaded into the R interpreter
+// TURNS OUT THAT THE EXPRESSION ARGUMENT (-e) MUST BE ENCLOSED IN DOUBLE QUOTES NOT SINGLE QUOTES!!
 
 int wmain(_In_ [[maybe_unused]] int argc, [[maybe_unused]] _In_ wchar_t* argv[]) {
     static const wchar_t* const         R_INTERPRETER_PATH { L"C:/R-4.5.2/bin/Rscript.exe" }; // the install directory of the R.exe binary
     static constexpr unsigned long long INTERPRETER_CMDLINE_BUFFSIZE { (1024 * 64) };         // being a bit too generous here
-
-    static constexpr unsigned long long CHILDPROC_STDOUT_PIPE_BUFFSIZE { 0xFFFFF };
-    std::string                         childproc_stdout_buffer;
-    childproc_stdout_buffer.reserve(CHILDPROC_STDOUT_PIPE_BUFFSIZE);
-
-    SYSTEM_INFO   sysinf { 0 };
-    unsigned long bfsize {};
+    SYSTEM_INFO                         sysinf { 0 };
 
     ::GetSystemInfo(&sysinf);
     ::wprintf_s(L"Number of processors: %lu\n", sysinf.dwNumberOfProcessors); // this machine has 18 cores, which is quite suprising
     // https://learn.microsoft.com/en-us/windows/win32/api/sysinfoapi/nf-sysinfoapi-getlogicalprocessorinformationex
 
-    wchar_t interpreter_invocation_cmdline[INTERPRETER_CMDLINE_BUFFSIZE] = { 0 };
-    // to invoke the R interpreter with strings passed as code, use the -e flag
-    // e.g. R.exe -e "write.csv(installed.packages(), file=\"name.csv\")"
-    // ::swprintf_s(interpreter_invocation_cmdline, INTERPRETER_CMDLINE_BUFFSIZE, R_INTERPRETER_PATH " -e \"\"");
-
     ::_putws(DUMMY_R_COMMANDLINE);
 
     PROCESS_INFORMATION childprocinfo {};
-    HANDLE64            childproc_stdout_write_end {} /* child's end */, childproc_stdout_read_end {} /* parent's end */;
-    SECURITY_ATTRIBUTES childsecattrs { .nLength = sizeof(SECURITY_ATTRIBUTES), .lpSecurityDescriptor = nullptr, .bInheritHandle = true };
-    STARTUPINFOW        childstarupinfo = {
-               .cb          = sizeof(STARTUPINFOW),
-               .lpTitle     = L"ChildProc",
-               .dwFlags     = STARTF_USESHOWWINDOW | STARTF_USESTDHANDLES | STARTF_FORCEONFEEDBACK,
-               .wShowWindow = SW_SHOW,
-               .hStdOutput  = childproc_stdout_write_end, // we are only interested in the stdouts of the child process
-               .hStdError   = childproc_stdout_write_end,
-    };
 
-    // if (!::CreatePipe(&childproc_stdout_write_end, &childproc_stdout_read_end, &childsecattrs, CHILDPROC_STDOUT_PIPE_BUFFSIZE))
-    //     ::fwprintf_s(stderr, L"%s error in call to CreatePipe!\n", ::error_code_to_string(::GetLastError()));
-    // else
-    //     // since we launch the child process after creating the pipe, make sure that the read end of the child process' stdout that the parent process has isn't inherited by the child process upon launch
-    //     if (!::SetHandleInformation(childproc_stdout_read_end, HANDLE_FLAG_INHERIT, false))
-    //         ::fwprintf_s(stderr, L"%s error in call to SetHandleInformation!\n", ::error_code_to_string(::GetLastError()));
+    STARTUPINFOW childstarupinfo = {
+        .cb          = sizeof(STARTUPINFOW),
+        .dwFlags     = STARTF_USESHOWWINDOW | STARTF_USESTDHANDLES | STARTF_FORCEONFEEDBACK,
+        .wShowWindow = SW_HIDE // hide the console window
+    };
 
     // https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-createprocessw
     // https://learn.microsoft.com/en-us/windows/win32/procthread/creating-processes
@@ -144,23 +150,9 @@ int wmain(_In_ [[maybe_unused]] int argc, [[maybe_unused]] _In_ wchar_t* argv[])
             &childstarupinfo,
             &childprocinfo
         )) {
-        ::fwprintf_s(stderr, L"%s error in call to CreateProcessW!\n", ::error_code_to_string(::GetLastError()));
+        ::fwprintf_s(stderr, L"%s error in call to CreateProcessW!\n", utils::error_code_to_string(::GetLastError()));
         return EXIT_FAILURE;
     }
-
-    // unsigned long readbytes_pipe {};
-    // long          read_status { true };
-    // for (;;) {
-    //     read_status = ::ReadFile(
-    //         childproc_stdout_read_end,
-    //         childproc_stdout_buffer.data() + readbytes_pipe, // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-    //         CHILDPROC_STDOUT_PIPE_BUFFSIZE,
-    //         &readbytes_pipe,
-    //         nullptr
-    //     );
-    //     if (!readbytes_pipe || !read_status) break;
-    // }
-    // ::WaitForSingleObject(childprocinfo.hProcess, INFINITE);
 
     switch (::WaitForSingleObject(childprocinfo.hProcess, INFINITE)) { // wait for the child process to finish
         case WAIT_ABANDONED :
@@ -174,7 +166,6 @@ int wmain(_In_ [[maybe_unused]] int argc, [[maybe_unused]] _In_ wchar_t* argv[])
 
     unsigned long childproc_exitcode = 0xFF;
 
-    ::wprintf_s(L"%S\n", childproc_stdout_buffer.c_str());
     ::GetExitCodeProcess(childprocinfo.hProcess, &childproc_exitcode);
     ::wprintf_s(L"Exit code of the child process is %lu\n", childproc_exitcode);
 
