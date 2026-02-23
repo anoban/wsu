39c1a1a7f0066689a9e58c3fed73490d05e083a1
anoban <2016sb117@univ.jfn.ac.lk>
2026-02-23 13:51:19 +1100

    modified:   R/traitmappedphylogenies.R

--- R/traitmappedphylogenies.R
+++ R/traitmappedphylogenies.R
@@ -143,14 +143,20 @@ dev.off()
 
 #------------------------------------------------------------------------
 # PHYLOGENY OF THE 995 SPECIES 5 MYCORRHIZAL STATE FRED V3 SUBSET
+# TO USE IN THE DRAFT FOR STEVEN SMITH & LUKE MCCORMACK
 #------------------------------------------------------------------------
 
 state_colors <- c("red", "blue", "yellow", "orange", "green")
 phylogeny <- ape::read.tree("../data/chapter2/uphylomaker/collab_fineroots_log_995_species_means_5states.tre")
+tscale_max <- max(phytools::nodeHeights(phylogeny))
 states <- read.csv("../data/chapter2/FREDv3subset/collab_fineroots_log_995_species_means_5states.csv")[, c("binominal", "state")]
 
 png("../plots/995_species_5states_mapped_phylogeny.png", width = 22000, height = 22000, units = "px", res = 400)
-plot <- phytools::plotTree(tree = phylogeny, ftype = "i", fsize = 1.0, type = "fan", lwd = 1, offset = 2)
+plot <- phytools::plotTree(tree = phylogeny, ftype = "i", fsize = 1.0, type = "fan", lwd = 1, offset = 2, part = 0.998)
+tscale_ticks <- seq(0, tscale_max, length.out = 20)
+tscale_axis <- axis(1, pos = -1, at = tscale_max - tscale_ticks, cex.axis = 1., labels = FALSE, col = "red")
+text(x = tscale_axis, y = rep(-3, length(tscale_ticks)), labels = lapply(rev(tscale_ticks), sprintf, fmt = "%.2f"), cex = 1, col = "red")
+text(x = tscale_max + 20, y = -2.5, labels = "Time (Million years)", cex = 1.00, col = "red")
 ape::tiplabels(pie = to.matrix(states$state, sort(unique(states$state))), piecol = state_colors, cex = 0.05) # nodes at the tips
 legend("topright", legend = sort(unique(states$state)), pt.bg = state_colors, cex = 3, pt.cex = 5, pch = 21, ncol = 1)
 dev.off()
