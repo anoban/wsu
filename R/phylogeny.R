42ce80b63e15d0efc9309d0d5e9c30695958364b
anoban <2016sb117@univ.jfn.ac.lk>
2026-02-23 11:20:13 +1100

    modified:   R/phylogeny.R 	modified:   data/chapter2/FREDv3subset/collab_fineroots_log_1006_species_means.csv 	modified:   data/chapter2/FREDv3subset/collab_fineroots_log_995_species_means_5states.csv 	new file:   data/chapter2/uphylomaker/collab_fineroots_log_995_species_means_5states.tre 	modified:   notebooks/chapter_02/populate_states.ipynb 	modified:   notebooks/chapter_02/subsetting.ipynb

--- R/phylogeny.R
+++ R/phylogeny.R
@@ -45,14 +45,19 @@ text(x = 250, y = -35, labels = "Time (Million years)", cex = 1.5, col = "red")
 dev.off()
 
 #------------------------------------------------------------------------------------------
-# THE NEW 994 SPECIES PHYLOGENY AFTER STATE CHANGES IN THE 1005 SPECIES PHYLOGENY
+# THE NEW 995 SPECIES PHYLOGENY AFTER STATE CHANGES IN THE 1005 SPECIES PHYLOGENY
+# 5 FINAL STATES WITH ErM AND OM SPECIES REMOVED
 #------------------------------------------------------------------------------------------
 
 megatree <- ape::read.tree("../data/chapter2/uphylomaker/GBOTB.extended.TPL.tre")
 taxonomy_data <- read.csv("../data/chapter2/FREDv3subset/collab_fineroots_log_995_species_means_5states.csv")
-phylogeny <- U.PhyloMaker::phylo.maker(sp.list = taxonomy_data[, c("binominal", "F01286")], tree = megatree,
-                                       gen.list = taxonomy_data[!duplicated(taxonomy_data[, c("F01286", "F01289")]), c("F01286", "F01289")])
-ape::write.tree(phylogeny$phylo, file = "../data/chapter2/uphylomaker/FRED_subset_collab_4states_994sp.tre")
+
+taxonomy_data$binominal <- gsub(taxonomy_data$binominal, pattern = ' ', replacement = '_')
+species_list <- data.frame(species = taxonomy_data$binominal, genus = taxonomy_data$F01286, family = taxonomy_data$F01289, species.relative = NA, genus.relative = NA)
+genus_list <- data.frame(genus = taxonomy_data$F01286, family = taxonomy_data$F01289)
+genus_list <- genus_list[!duplicated(genus_list), ] # drop the duplicates
+
+phylogeny <- U.PhyloMaker::phylo.maker(sp.list = species_list, tree = megatree, gen.list = genus_list)
+ape::write.tree(phylogeny$phylo, file = "../data/chapter2/uphylomaker/collab_fineroots_log_995_species_means_5states.tre")
 
 
-#
