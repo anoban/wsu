2dc797e7a2812dab08cf3aaa683fea015a96daa8
anoban <2016sb117@univ.jfn.ac.lk>
2026-03-12 11:10:03 +1100

    modified:   R/phylogeny.R 	new file:   R/wrangling.R 	modified:   data/chapter2/FREDv3subset/collab_fineroots_log_995_species_means_5states.csv 	modified:   notebooks/chapter_02/populate_states.ipynb

--- R/phylogeny.R
+++ R/phylogeny.R
@@ -44,24 +44,3 @@ text(x = tscale_axis, y = rep(-16, 10), labels = lapply(rev(seq(0, htree, length
 text(x = 250, y = -35, labels = "Time (Million years)", cex = 1.5, col = "red")
 dev.off()
 
-#------------------------------------------------------------------------------------------
-# THE NEW 995 SPECIES PHYLOGENY AFTER STATE CHANGES IN THE 1005 SPECIES PHYLOGENY
-# 5 FINAL STATES WITH ErM AND OM SPECIES REMOVED
-#------------------------------------------------------------------------------------------
-
-megatree <- ape::read.tree("../data/chapter2/uphylomaker/GBOTB.extended.TPL.tre")
-taxonomy_data <- read.csv("../data/chapter2/FREDv3subset/collab_fineroots_log_995_species_means_5states.csv")
-
-# taxonomy_data$binominal <- gsub(taxonomy_data$binominal, pattern = ' ', replacement = '_')
-species_list <- data.frame(species = taxonomy_data$binominal, genus = taxonomy_data$F01286, family = taxonomy_data$F01289, species.relative = NA, genus.relative = NA)
-genus_list <- data.frame(genus = taxonomy_data$F01286, family = taxonomy_data$F01289)
-genus_list <- genus_list[!duplicated(genus_list), ] # drop the duplicates
-
-phylogeny <- U.PhyloMaker::phylo.maker(sp.list = species_list, tree = megatree, gen.list = genus_list)
-ape::write.tree(phylogeny$phylo, file = "../data/chapter2/uphylomaker/collab_fineroots_log_995_species_means_5states.tre")
-
-# name match the dataset
-phylogeny <- ape::read.tree("../data/chapter2/uphylomaker/collab_fineroots_log_995_species_means_5states.tre")
-taxonomy_data <- taxonomy_data[match(phylogeny$tip.label, taxonomy_data$binominal), ]
-stopifnot(taxonomy_data$binominal == phylogeny$tip.label)
-write.csv(x = taxonomy_data, file = "../data/chapter2/FREDv3subset/collab_fineroots_log_995_species_means_5states_name_matched_with_phylogeny.csv", row.names = FALSE)
