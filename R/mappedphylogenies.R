79cf586d2b33c30fcbe79bafb2147626d19419cd
anoban <2016sb117@univ.jfn.ac.lk>
2026-06-30 14:07:17 +1000

    modified:   .gitmodules 	modified:   R/mappedphylogenies.R 	modified:   R/notebooks/plot-phylogenies.ipynb 	deleted:    R/thirdparty/ggtree 	deleted:    R/thirdparty/treeio

--- R/mappedphylogenies.R
+++ R/mappedphylogenies.R
@@ -15,13 +15,21 @@ stopifnot(all(data$binominal == phylogeny$tip.label))
 # phylogeny visualization
 #--------------------------
 
+# filter the edges that end at tips
+all_edges <- data.frame(phylogeny$edge)
+all_edges$colour <- NA # introduce a colour column
+# update the colour column with states which can later be remapped to colours
+all_edges[phylogeny$edge[, 2] %in% 1:length(phylogeny$tip.label), "colour"] <- data$state[all_edges[phylogeny$edge[, 2] %in% 1:length(phylogeny$tip.label), "X2"]]
+STATE_COLOURS <- c(AM = "darkgreen", EcM = "brown", ErM = "#5A778E", NMAM = "#483023", EcMAM = "#FF00FF")
+all_edges$colour <- STATE_COLOURS[all_edges$colour]
+all_edges$colour[is.na(all_edges$colour)] <- "#CDCBCE"
+
 png("../plots/phylogeny.png", width = 22000, height = 22000, units = "px", res = 400)#, bg = "transparent")
-ape::plot.phylo(x = phylogeny, ftype = "i", fsize = 0.80, lwd = 1, offset = 1, part = 1, type = "fan", show.tip.label = FALSE, show.node.label = FALSE)
-ape::tiplabels(tip = seq_along(phylogeny$tip.label)[data$state == "AM"], col = "red", pch=19, cex = 1.5, offset = 2)
-ape::tiplabels(tip = seq_along(phylogeny$tip.label)[data$state == "EcM"], col = "darkgreen", pch=19, cex = 1.5, offset = 4)
-ape::tiplabels(tip = seq_along(phylogeny$tip.label)[data$state == "NM"], col = "blue", pch=19, cex = 1.5, offset = 4)
-ape::tiplabels(tip = seq_along(phylogeny$tip.label)[data$state == "ErM"], col = "brown", pch=19, cex = 1.5, offset = 4)
-ape::tiplabels(tip = seq_along(phylogeny$tip.label)[data$state == "EcMAM"], col = "orange", pch=19, cex = 1.5, offset = 4)
-ape::tiplabels(tip = seq_along(phylogeny$tip.label)[data$state == "NMAM"], col = "purple", pch=19, cex = 1.5, offset = 4)
+ape::plot.phylo(x = phylogeny, lwd = 1, type = "fan", show.tip.label = FALSE, show.node.label = FALSE, edge.color = all_edges$colour)
+ape::tiplabels(bg = "red", pch=21, cex = scale(exp(data$F00679), center = FALSE), offset = 1.5) # RD
+ape::tiplabels(bg = "blue", pch=21, cex = scale(exp(data$F00727), center = FALSE), offset = 5.5) # SRL
+ape::tiplabels(bg = "orange", pch=21, cex = scale(exp(data$F00709), center = FALSE), offset = 9.5) # RTD
 dev.off()
+
+
 
