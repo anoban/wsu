bfbd7fd5fe8255f7fbb0e9760c640d6f8f5c9d66
anoban <2016sb117@univ.jfn.ac.lk>
2026-07-01 12:52:04 +1000

    modified:   R/installedpackages.R 	modified:   R/notebooks/brms.ipynb 	modified:   R/notebooks/phylogenetics.ipynb 	renamed:    R/packages.txt -> installed.packages.csv

--- R/installedpackages.R
+++ R/installedpackages.R
@@ -1,1 +1,1 @@
-write.csv(x = installed.packages()[, c("Package", "Version")], file = "./packages.txt", row.names = FALSE, fileEncoding = "ascii")
+write.csv(x = installed.packages()[, c("Package", "Version")], file = "./../installed.packages.csv", row.names = FALSE, fileEncoding = "ascii")
