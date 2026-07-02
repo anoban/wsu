677a173924cf278320329f0287b953f532e0826d
anoban <2016sb117@univ.jfn.ac.lk>
2026-07-02 14:49:09 +1000

    modified:   R/getModelAvgParams.R 	modified:   R/notebooks/brms.ipynb 	modified:   R/notebooks/houwiesummary.ipynb

--- R/getModelAvgParams.R
+++ R/getModelAvgParams.R
@@ -1,6 +1,39 @@
 library("OUwie")
 
-getModelAveragedParams <- function(model.list, BM_alpha_treatment="zero", type="AICc", force=FALSE){
+getModTable <- function(model.list, type="BIC"){
+    # checks
+    if(!inherits(model.list, what="list")){
+        #if(class(model.list) != "list"){
+        stop("Input object must be of class list with each element as a separet fit model to the same dataset.", call. = FALSE)
+    }
+
+    if(!all(unlist(lapply(model.list, function(x) class(x))) == "houwie")){
+        warning("Not all models are of class houwie. These have been removed.")
+        model.list <- model.list[unlist(lapply(model.list, function(x) class(x)) == "houwie")]
+    }
+
+    if(length(model.list) == 1){
+        stop("Two or models are needed to conduct model averaging.", call. = FALSE)
+    }
+
+    if(var(unlist(lapply(model.list, function(x) dim(x$data)[1]))) != 0){
+        stop("The number of rows in your data are not the same for all models. Models should not be compared if they are not evaluating the same dataset.", call.=FALSE)
+    }
+
+    ParCount <- unlist(lapply(model.list, function(x) x$param.count))
+    nTip <- length(model.list[[1]]$phy$tip.label)
+    AIC <- simplify2array(lapply(model.list, "[[", type))
+    dAIC <- AIC - min(AIC)
+    AICwt <- exp(-0.5 * dAIC)/sum(exp(-0.5 * dAIC))
+    LogLik <- simplify2array(lapply(model.list, "[[", "loglik"))
+    DiscLik <- simplify2array(lapply(model.list, "[[", "DiscLik"))
+    ContLik <- simplify2array(lapply(model.list, "[[", "ContLik"))
+    model_table <- data.frame(np = ParCount, lnLik = LogLik, DiscLik=DiscLik, ContLik=ContLik, AIC = AIC, dAIC = dAIC, AICwt = AICwt)
+    colnames(model_table) <- gsub("AIC", type, colnames(model_table))
+    return(model_table)
+}
+
+getModelAveragedParams <- function(model.list, BM_alpha_treatment="zero", type="AICc", force=TRUE){
 
     if(any(unlist(lapply(model.list, class))!="houwie")){
         warning("Some of the input models are not of class houwie, these have been removed.")
@@ -25,16 +58,16 @@ getModelAveragedParams <- function(model.list, BM_alpha_treatment="zero", type="
     }
 
     # pull the aic weights
-    print("line 28")
-    mods_table <- OUwie::getModelTable(model.list, type=type)
+    # print("line 28")
+    mods_table <- getModTable(model.list, type=type)
     # print(mods_table)
-    if(diff(range(mods_table[,5])) > 1e5){
+    if(diff(range(mods_table[,5])) > 1e10){
         if(!force){
             max_aic <- max(mods_table[,5])
-            model.list <- model.list[abs(mods_table[,5] - max_aic)  < 1e5]
-            print(paste0("The size of model.list after removing the failed models is now ", length(model.list)))
-            print("line 34")
-            mods_table <- OUwie::getModelTable(model.list, type=type)
+            model.list <- model.list[abs(mods_table[,5] - max_aic)  < 1e10]
+            warning(paste0("The length of model.list after removing the failed models is now ", length(model.list)))
+            # print("line 34")
+            mods_table <- getModTable(model.list, type=type)
             mod_names <- names(model.list)
         }else{
             warning("It is possible that one or more of your models failed to converge. The AIC between the best and worst models exceeds 1e10. Set force=FALSE to automatically remove potentially failed runs.")
