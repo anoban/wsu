39af10b40287f3ac51d957f5be4880f51fdbe0c0
anoban <2016sb117@univ.jfn.ac.lk>
2026-08-11 08:49:22 +1000

    new file:   R/dunnTest.R 	modified:   R/notebooks/phylogenetics.ipynb 	deleted:    R/phylogeneticANOVA.R

---
+++ R/dunnTest.R
@@ -0,0 +1,23 @@
+library("FSA")
+
+fred4_raw <- read.csv("../data/chapter2/FRED/subsets/continuous_raw.csv")
+fred4_raw[, 5:7] <- fred4_raw[, 5:7] |> log() |> scale()
+
+dunnRD <- FSA::dunnTest(F00679~as.factor(binominal), data = fred4_raw, method = "holm");
+dunnSRL <- FSA::dunnTest(F00727~as.factor(binominal), data = fred4_raw, method = "holm");
+dunnRTD <- FSA::dunnTest(F00709~as.factor(binominal), data = fred4_raw, method = "holm");
+
+save(dunnRD, dunnSRL, dunnRTD, file = "../data/chapter2/rdata/dunnTest.RData")
+
+load("../data/chapter2/rdata/dunnTest.RData")
+
+# all the pairwise species comparisons
+dunnRD$res$Comparison
+
+length(dunnRD$res$Comparison) == length(unique(dunnRD$res$Comparison)) # damn
+length(dunnRD$res$Comparison)
+
+# species pairs with adjusted P value < 0.05
+subset(dunnRD$res, P.adj < 0.05)
+
+subset(dunnRD$res, P.adj < 0.05) |> dim() # 485 species pairs
