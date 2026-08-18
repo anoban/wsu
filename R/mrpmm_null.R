4f247822877c4de27eba34ddcbee335556ff4890
anoban <2016sb117@univ.jfn.ac.lk>
2026-08-18 10:04:53 +1000

    deleted:    R/mrpmm_lambda.R 	new file:   R/mrpmm_null.R 	modified:   R/notebooks/phylogenetics.ipynb

---
+++ R/mrpmm_null.R
@@ -0,0 +1,39 @@
+#!/usr/bin/env Rscript
+
+library("ape")
+library("brms")
+library("readxl")
+library("geiger")
+
+fred4 <- read.csv("./ScratchData/continuous_raw.csv") # continuous trait data with all the raw records
+fred4$taxa <- fred4$binominal # duplicated column to be fitted as the random effect
+
+# log transformation and standardization of the continuous traits
+fred4$F00679 <- scale(log(fred4$F00679))[, 1] # RD
+fred4$F00727 <- scale(log(fred4$F00727))[, 1] # SRL
+fred4$F00709 <- scale(log(fred4$F00709))[, 1] # RTD
+
+
+tree <- ape::read.tree("./ScratchData/FRED4_1301.tre") # the phylogeny
+if(!ape::is.binary(tree)) tree <- ape::multi2di(tree) # if not binary, make it binary
+stopifnot(all(tree$tip.label %in% fred4$binominal))
+
+corrmat <- ape::vcv.phylo(phy = tree, corr = TRUE) # turn the phylogeny into a variance-covariance matrix based on branch lengths
+
+# null models - without mycorrhizal states as the fixed effect
+M1 <- brms::brm(brms::brmsformula(mvbind(F00727, F00679, F00709) ~ 1 + (1|gr(binominal, cov = corrmat)) + (1|taxa)) + set_rescor(TRUE),
+                data = fred4, data2 = list(corrmat = corrmat), chains = 8, cores = 8, threads = 2, iter = 5000, warmup = 2500, backend = "cmdstanr")
+saveRDS(object = M1, file = "./ScratchData/nullmods/M1_null.Rds")
+
+M2 <- brms::brm(brms::brmsformula(mvbind(F00727, F00679, F00709) ~ 1 + (1|taxa)) + set_rescor(TRUE), data = fred4, data2 = list(corrmat = corrmat),
+          chains = 8, cores = 8, threads = 2, iter = 5000, warmup = 2500, backend = "cmdstanr")
+saveRDS(object = M2, file = "./ScratchData/nullmods/M2_null.Rds")
+
+M4 <- brms::brm(brms::brmsformula(mvbind(F00727, F00679, F00709) ~ 1 + (1|q|taxa)) + set_rescor(TRUE), data = fred4, data2 = list(corrmat = corrmat),
+          chains = 8, cores = 8, threads = 2, iter = 5000, warmup = 2500, backend = "cmdstanr")
+saveRDS(object = M4, file = "./ScratchData/nullmods/M4_null.Rds")
+
+# will probably be the slowest - moving to the last
+M3 <- brms::brm(brms::brmsformula(mvbind(F00727, F00679, F00709) ~ 1 + (1|p|gr(binominal, cov = corrmat)) + (1|q|taxa)) + set_rescor(TRUE),
+                data = fred4, data2 = list(corrmat = corrmat), chains = 8, cores = 8, threads = 2, iter = 5000, warmup = 2500, backend = "cmdstanr")
+saveRDS(object = M3, file = "./ScratchData/nullmods/M3_null.Rds")
