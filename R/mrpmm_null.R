f21d921921ad84478f58a2fdf94b3b0e83bf6bb6
anoban <2016sb117@univ.jfn.ac.lk>
2026-08-20 09:38:20 +1000

    modified:   R/mrpmm_null.R 	modified:   R/notebooks/phylogenetics.ipynb

--- R/mrpmm_null.R
+++ R/mrpmm_null.R
@@ -1,42 +1,49 @@
-#!/usr/bin/env Rscript
-
 library("ape")
 library("brms")
 library("cmdstanr")
-cmdstanr::set_cmdstan_path(cmdstanr::cmdstan_default_path())
 
-fred4 <- read.csv("./ScratchData/continuous_raw.csv") # continuous trait data with all the raw records
-fred4$taxa <- fred4$binominal # duplicated column to be fitted as the random effect
+fred4 <- read.csv("./ScratchData/continuous_raw.csv")
+fred4$taxa <- fred4$binominal
 
-# log transformation and standardization of the continuous traits
-fred4$F00679 <- scale(log(fred4$F00679))[, 1] # RD
-fred4$F00727 <- scale(log(fred4$F00727))[, 1] # SRL
-fred4$F00709 <- scale(log(fred4$F00709))[, 1] # RTD
+fred4$F00679 <- scale(log(fred4$F00679))[, 1]
+fred4$F00727 <- scale(log(fred4$F00727))[, 1]
+fred4$F00709 <- scale(log(fred4$F00709))[, 1]
 
-tree <- ape::read.tree("./ScratchData/FRED4_1301.tre") # the phylogeny
-if(!ape::is.binary(tree)) tree <- ape::multi2di(tree) # if not binary, make it binary
+tree <- ape::read.tree("./ScratchData/FRED4_1301.tre")
+if(!ape::is.binary(tree)) tree <- ape::multi2di(tree)
 stopifnot(all(tree$tip.label %in% fred4$binominal))
 
-corrmat <- ape::vcv.phylo(phy = tree, corr = TRUE) # turn the phylogeny into a variance-covariance matrix based on branch lengths
+corrmat <- ape::vcv.phylo(phy = tree, corr = TRUE)
 
-# null models - without mycorrhizal states as the fixed effect
-M2 <- brms::brm(brms::brmsformula(mvbind(F00727, F00679, F00709) ~ 1 + (1|taxa)) + set_rescor(TRUE), data = fred4, data2 = list(corrmat = corrmat),
-          chains = 8, cores = 8, threads = 4, iter = 5000, warmup = 2500, backend = "cmdstanr")
-saveRDS(object = M2, file = "./ScratchData/nullmods/M2_null.Rds")
+# let's see if the try catch blocks help with unexpected chain terminations
+tryCatch(
+    expr = {
+        M2 <- brms::brm(brms::brmsformula(mvbind(F00727, F00679, F00709) ~ 1 + (1|taxa)) + set_rescor(TRUE), data = fred4, chains = 8, cores = 8, threads = 4, iter = 5000, warmup = 2500, backend = "cmdstanr")
+        saveRDS(object = M2, file = "./ScratchData/nullmods/brms_taxa_null.Rds")
+    },
+    error = function(err){ print(err) }
+)
 
-M4 <- brms::brm(brms::brmsformula(mvbind(F00727, F00679, F00709) ~ 1 + (1|q|taxa)) + set_rescor(TRUE), data = fred4, data2 = list(corrmat = corrmat),
-          chains = 8, cores = 8, threads = 4, iter = 5000, warmup = 2500)
-saveRDS(object = M4, file = "./ScratchData/nullmods/M4_null.Rds")
+tryCatch(
+    expr = {
+        M4 <- brms::brm(brms::brmsformula(mvbind(F00727, F00679, F00709) ~ 1 + (1|q|taxa)) + set_rescor(TRUE), data = fred4, chains = 8, cores = 8, threads = 4, iter = 5000, warmup = 2500, backend = "cmdstanr")
+        saveRDS(object = M4, file = "./ScratchData/nullmods/brms_taxa_corr_null.Rds")
+    },
+    error = function(err){ print(err) }
+)
 
-M1 <- brms::brm(brms::brmsformula(mvbind(F00727, F00679, F00709) ~ 1 + (1|gr(binominal, cov = corrmat)) + (1|taxa)) + set_rescor(TRUE),
-                data = fred4, data2 = list(corrmat = corrmat), chains = 8, cores = 8, threads = 4, iter = 5000, warmup = 2500, backend = "cmdstanr")
-saveRDS(object = M1, file = "./ScratchData/nullmods/M1_null.Rds")
+tryCatch(
+    expr = {
+        M1 <- brms::brm(brms::brmsformula(mvbind(F00727, F00679, F00709) ~ 1 + (1|gr(binominal, cov = corrmat)) + (1|taxa)) + set_rescor(TRUE), data = fred4, data2 = list(corrmat = corrmat), chains = 8, cores = 8, threads = 4, iter = 5000, warmup = 2500, backend = "cmdstanr")
+        saveRDS(object = M1, file = "./ScratchData/nullmods/brms_phylo_null.Rds")
+    },
+    error = function(err){ print(err) }
+)
 
-# will probably be the slowest - moving to the last
-M3 <- brms::brm(brms::brmsformula(mvbind(F00727, F00679, F00709) ~ 1 + (1|p|gr(binominal, cov = corrmat)) + (1|q|taxa)) + set_rescor(TRUE),
-                data = fred4, data2 = list(corrmat = corrmat), chains = 8, cores = 8, threads = 4, iter = 5000, warmup = 2500, backend = "cmdstanr")
-saveRDS(object = M3, file = "./ScratchData/nullmods/M3_null.Rds")
-
-fred4 <- read.csv("../data/chapter2/FRED/subsets/continuous_raw.csv")
-fred4$taxa <- fred4$binominal
-tree <- ape::read.tree("../data/chapter2/uphylomaker/FRED4_1301.tre")
+tryCatch(
+    expr = {
+        M3 <- brms::brm(brms::brmsformula(mvbind(F00727, F00679, F00709) ~ 1 + (1|p|gr(binominal, cov = corrmat)) + (1|q|taxa)) + set_rescor(TRUE), data = fred4, data2 = list(corrmat = corrmat), chains = 8, cores = 8, threads = 4, iter = 5000, warmup = 2500, backend = "cmdstanr")
+        saveRDS(object = M3, file = "./ScratchData/nullmods/brms_phylo_corr_null.Rds")
+    },
+    error = function(err){ print(err) }
+)
